c1673af71199f5b3df51c3c22baee4ab6cae8191
David <davidfarias@blindariesgos.com>
2024-04-03 14:28:02 -0400

    video tutorial y class a className

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -5,25 +5,15 @@ import GetPassword from "./components/GetPassword";
 import ChangePassword from "./components/ChangePassword";
 import DontRememberDetails from "./components/DontRememberDetails";
 import CheckUser from "./components/CheckUser";
-import ModalVideo from "./components/ModalVideo";
 
 export default function Page() {
   return (
-    <div style={{ backgroundColor: "#DFE3E6", borderRadius: "20px" }} class="w-auto py-7 px-6">
-      {/* <Login /> */}
+    <div style={{ backgroundColor: "#DFE3E6", borderRadius: "20px" }} className="w-auto py-7 px-6">
+      <Login />
       {/* <GetPassword /> */}
       {/* <ChangePassword /> */}
       {/* <DontRememberDetails /> */}
-      <CheckUser />
-      <ModalVideo buttonOpen={
-        <button
-          type="button"
-          onClick={() => setIsOpen(true)}
-          className="px-4 py-2 text-white bg-blue-500 rounded"
-        >
-          Abrir Modal
-        </button>
-      } />
+      {/* <CheckUser /> */}
     </div >
   )
 }

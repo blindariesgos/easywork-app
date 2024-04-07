14a54ba9a7495504388a5c0f7a68f77f6fbaba7f
Rosmer <criptopreto@gmail.com>
2024-04-07 00:32:35 -0400

    Agregar dockerfile

---
+++ docker/dev.Dockerfile
@@ -0,0 +1,20 @@
+# Utiliza una imagen base de Node.js con la versión deseada
+FROM node:21-alpine
+
+# Establece el directorio de trabajo en la carpeta de la aplicación
+WORKDIR /app
+
+# Copia los archivos de configuración necesarios (package.json y package-lock.json) a la carpeta de trabajo
+COPY package.json package-lock.json ./
+
+# Instala las dependencias de la aplicación
+RUN npm install
+
+# Copia el resto de los archivos de la aplicación a la carpeta de trabajo
+COPY . .
+
+# Expone el puerto en el que se ejecuta la aplicación de Next.js
+EXPOSE 3000
+
+# Define el comando para iniciar la aplicación en modo de desarrollo
+CMD ["npm", "run", "dev"]
--- src/app/[locale]/(home)/sales/crm/contacts/components/FormFilters.jsx
+++ src/app/[locale]/(home)/sales/crm/contacts/components/FormFilters.jsx
@@ -1,102 +1,104 @@
-import Button from '@/components/form/Button';
-import React, { useEffect, useState } from 'react';
-import MultipleSelect from './Test';
-import SelectDropdown from './create_contact/SelectDropdown';
-import { useForm, Controller } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import { useTranslation } from 'react-i18next';
-import { filterOptions, responsible } from '@/lib/common';
-import { FaMagnifyingGlass } from 'react-icons/fa6';
-import { useCommon } from '@/hooks/useCommon';
-import SelectInput from '@/components/form/SelectInput';
-import TextInput from '@/components/form/TextInput';
-import {
-	PlusIcon,
-  } from "@heroicons/react/20/solid";
-  import DatePicker from "react-datepicker";
-  import "react-datepicker/dist/react-datepicker.css";
-
+import Button from "@/components/form/Button";
+import React, { useEffect, useState } from "react";
+import SelectDropdown from "./create_contact/SelectDropdown";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useTranslation } from "react-i18next";
+import { filterOptions, responsible } from "@/lib/common";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { useCommon } from "@/hooks/useCommon";
+import SelectInput from "@/components/form/SelectInput";
+import TextInput from "@/components/form/TextInput";
+import { PlusIcon } from "@heroicons/react/20/solid";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const contactSources = [
-	{ id: 1, name: 'Correo electrónico' },
-	{ id: 2, name: 'Maratón de llamadas' },
-	{ id: 3, name: 'Formulario de CRM' },
-	{ id: 4, name: 'Formulario de devolución de llamada' },
-	{ id: 5, name: 'Gestión del agente' },
-	{ id: 6, name: 'Red social - LinkedIn' },
-	{ id: 7, name: 'Red social - Instagram' },
-	{ id: 8, name: 'Red social - Facebook' },
-	{ id: 9, name: 'Red social - Otra' },
-	{ id: 10, name: 'Otro CRM' },
-	{ id: 11, name: 'Página de ventas' },
-	{ id: 12, name: 'Teléfono' },
-	{ id: 13, name: 'WhatsApp' }
+  { id: 1, name: "Correo electrónico" },
+  { id: 2, name: "Maratón de llamadas" },
+  { id: 3, name: "Formulario de CRM" },
+  { id: 4, name: "Formulario de devolución de llamada" },
+  { id: 5, name: "Gestión del agente" },
+  { id: 6, name: "Red social - LinkedIn" },
+  { id: 7, name: "Red social - Instagram" },
+  { id: 8, name: "Red social - Facebook" },
+  { id: 9, name: "Red social - Otra" },
+  { id: 10, name: "Otro CRM" },
+  { id: 11, name: "Página de ventas" },
+  { id: 12, name: "Teléfono" },
+  { id: 13, name: "WhatsApp" },
 ];
 
 const FormFilters = () => {
-	const { t } = useTranslation();
-	const { createdDate } = useCommon();
-	const [ queryResponsible, setQueryResponsible ] = useState('');
-	const [ queryDate, setQueryDate ] = useState('');
-	const [ querySource, setQuerySource ] = useState('');
-    const [contactSource, setContactSource] = useState(null);
-	const [ queryCreated, setQueryCreated ] = useState('');
-	const filteredContactResponsible = filterOptions(queryResponsible, responsible);
-	const filteredContactDate = filterOptions(queryDate, createdDate);
-	const filteredContactSources = filterOptions(querySource, contactSources);
-	const filteredContactCreated = filterOptions(queryCreated, responsible);
-	const schema = yup.object().shape({
-		// responsible: yup.array().min(1, 'Al menos una opción debe seleccionarse'),
-		responsible: yup.string(),
-		date: yup.object(),
-        origin: yup.string(),
-		createdBy: yup.string(),
-		ndays: yup.number(),
-		month: yup.string(),
-		quarter: yup.string(),
-		year: yup.string(),
-		exactDate: yup.string(),
-		// range: yup.array().of(yup.string().required()).required().min(2).max(2),
-	});
-	const [dateRange, setDateRange] = useState([null, null]);
-	const [startDate, endDate] = dateRange;
-    const {
-        register,
-        handleSubmit,
-        control,
-        reset,
-        setValue,
-        getValues,
-		watch,
-        formState: { isValid, errors },
-      } = useForm({
-          defaultValues: {
-			range: [null, null],
-          },
-          mode: "onChange",
-          resolver: yupResolver(schema),
-      });
+  const { t } = useTranslation();
+  const { createdDate } = useCommon();
+  const [queryResponsible, setQueryResponsible] = useState("");
+  const [queryDate, setQueryDate] = useState("");
+  const [querySource, setQuerySource] = useState("");
+  const [contactSource, setContactSource] = useState(null);
+  const [queryCreated, setQueryCreated] = useState("");
+  const filteredContactResponsible = filterOptions(
+    queryResponsible,
+    responsible
+  );
+  const filteredContactDate = filterOptions(queryDate, createdDate);
+  const filteredContactSources = filterOptions(querySource, contactSources);
+  const filteredContactCreated = filterOptions(queryCreated, responsible);
+  const schema = yup.object().shape({
+    // responsible: yup.array().min(1, 'Al menos una opción debe seleccionarse'),
+    responsible: yup.string(),
+    date: yup.object(),
+    origin: yup.string(),
+    createdBy: yup.string(),
+    ndays: yup.number(),
+    month: yup.string(),
+    quarter: yup.string(),
+    year: yup.string(),
+    exactDate: yup.string(),
+    // range: yup.array().of(yup.string().required()).required().min(2).max(2),
+  });
+  const [dateRange, setDateRange] = useState([null, null]);
+  const [startDate, endDate] = dateRange;
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    setValue,
+    getValues,
+    watch,
+    formState: { isValid, errors },
+  } = useForm({
+    defaultValues: {
+      range: [null, null],
+    },
+    mode: "onChange",
+    resolver: yupResolver(schema),
+  });
 
-	const handleFormFilters = (data) => {
-		console.log('data', data);
-	};
+  const handleFormFilters = (data) => {
+    console.log("data", data);
+  };
 
-	useEffect(() => {
-		reset();
-	}, []);
+  useEffect(() => {
+    reset();
+  }, []);
 
-	// console.log("ccf", getValues())
-	return (
-		<form onSubmit={handleSubmit(handleFormFilters)} className="grid grid-cols-1 gap-2 sm:w-96 w-72 px-2">
-			<SelectDropdown
-				label={t('contacts:filters:responsible')}
-				name="responsible"
-				options={filteredContactResponsible}
-				onChangeInput={setQueryResponsible}
-				setValue={setValue}
-			/>
-			{/* <Controller
+  // console.log("ccf", getValues())
+  return (
+    <form
+      onSubmit={handleSubmit(handleFormFilters)}
+      className="grid grid-cols-1 gap-2 sm:w-96 w-72 px-2"
+    >
+      <SelectDropdown
+        label={t("contacts:filters:responsible")}
+        name="responsible"
+        options={filteredContactResponsible}
+        onChangeInput={setQueryResponsible}
+        setValue={setValue}
+      />
+      {/* <Controller
 				name="responsible"
 				control={control}
 				defaultValue={[]}
@@ -112,103 +114,99 @@ const FormFilters = () => {
 					/>
 				)}
 			/> */}
-			<div className='flex gap-2 items-end'>
-				<div className={`${watch("date")?.date ? "w-2/5" : "w-full"}`}>
-					<SelectInput
-						label={t('contacts:filters:created')}
-						name="date"
-						options={filteredContactDate}
-						onChangeInput={setQueryDate}
-						setValue={setValue}
-						object={true}
-					/>
-				</div>
-				{watch("date")?.date === "input" && (
-					<div className='w-3/5'>
-						<TextInput
-							type="number"		
-							name="ndays"
-							register={register}				
-						/>
-					</div>
-				)}
-				{watch("date")?.date === "month" && (
-					<div className='w-3/5 flex gap-2'>
-						<DatePicker
-							selected={watch("month")}
-							onChange={(date) => setValue("month", date)}
-							showMonthYearPicker
-							dateFormat="MM/yyyy"
-							className='w-full border-none drop-shadow-sm h-9 rounded-md text-sm focus:ring-0'
-    					/>
-					</div>
-				)}
-				{watch("date")?.date === "quarter" && (
-					<div className='w-3/5 flex gap-2'>
-						<DatePicker
-							selected={watch("quarter")}
-							onChange={(date) => setValue("quarter", date)}
-							showQuarterYearPicker
-							dateFormat="yyyy, QQQ"
-							className='w-full border-none drop-shadow-sm h-9 rounded-md text-sm focus:ring-0'
-    					/>
-					</div>
-				)}
-				{watch("date")?.date === "year" && (
-					<div className='w-3/5 flex gap-2'>
-						<DatePicker
-							selected={watch("year")}
-							onChange={(date) => setValue("year", date)}
-							showYearPicker
-							dateFormat="yyyy"
-							className='w-full border-none drop-shadow-sm h-9 rounded-md text-sm focus:ring-0'
-    					/>
-					</div>
-				)}
-				{watch("date")?.date === "exactDate" && (
-					<div className='w-3/5 flex gap-2'>
-						<DatePicker
-							selected={watch("exactDate")}
-							onChange={(date) => setValue("exactDate", date)}
-							className='w-full border-none drop-shadow-sm h-9 rounded-md text-sm focus:ring-0'
-    					/>
-					</div>
-				)}
-				{watch("date")?.date === "range" && (
-					<div className='w-3/5 flex gap-2'>
-						<DatePicker
-							selectsRange={true}
-							startDate={startDate}
-							endDate={endDate}
-							onChange={(update) => {
-								setDateRange(update);
-							}}
-							isClearable={true}
-							className='w-full border-none drop-shadow-sm h-9 rounded-md text-sm focus:ring-0'
-					 	/>
-					</div>
-				)}
-			</div>
-			<Controller
-				name="origin"
-				control={control}
-				defaultValue={""}
-				render={({ field }) => {
-					return (
-						<SelectInput
-							{...field}
-							label={t('contacts:filters:origin')}
-							name="origin"
-							options={filteredContactSources}
-							selectedOption={contactSource}
-							onChangeInput={setQuerySource}
-							error={!field.value && errors.origin}
-							setValue={setValue}
-						/>
-					)
-				}}
-			/>
-			{/* <Controller
+      <div className="flex gap-2 items-end">
+        <div className={`${watch("date")?.date ? "w-2/5" : "w-full"}`}>
+          <SelectInput
+            label={t("contacts:filters:created")}
+            name="date"
+            options={filteredContactDate}
+            onChangeInput={setQueryDate}
+            setValue={setValue}
+            object={true}
+          />
+        </div>
+        {watch("date")?.date === "input" && (
+          <div className="w-3/5">
+            <TextInput type="number" name="ndays" register={register} />
+          </div>
+        )}
+        {watch("date")?.date === "month" && (
+          <div className="w-3/5 flex gap-2">
+            <DatePicker
+              selected={watch("month")}
+              onChange={(date) => setValue("month", date)}
+              showMonthYearPicker
+              dateFormat="MM/yyyy"
+              className="w-full border-none drop-shadow-sm h-9 rounded-md text-sm focus:ring-0"
+            />
+          </div>
+        )}
+        {watch("date")?.date === "quarter" && (
+          <div className="w-3/5 flex gap-2">
+            <DatePicker
+              selected={watch("quarter")}
+              onChange={(date) => setValue("quarter", date)}
+              showQuarterYearPicker
+              dateFormat="yyyy, QQQ"
+              className="w-full border-none drop-shadow-sm h-9 rounded-md text-sm focus:ring-0"
+            />
+          </div>
+        )}
+        {watch("date")?.date === "year" && (
+          <div className="w-3/5 flex gap-2">
+            <DatePicker
+              selected={watch("year")}
+              onChange={(date) => setValue("year", date)}
+              showYearPicker
+              dateFormat="yyyy"
+              className="w-full border-none drop-shadow-sm h-9 rounded-md text-sm focus:ring-0"
+            />
+          </div>
+        )}
+        {watch("date")?.date === "exactDate" && (
+          <div className="w-3/5 flex gap-2">
+            <DatePicker
+              selected={watch("exactDate")}
+              onChange={(date) => setValue("exactDate", date)}
+              className="w-full border-none drop-shadow-sm h-9 rounded-md text-sm focus:ring-0"
+            />
+          </div>
+        )}
+        {watch("date")?.date === "range" && (
+          <div className="w-3/5 flex gap-2">
+            <DatePicker
+              selectsRange={true}
+              startDate={startDate}
+              endDate={endDate}
+              onChange={(update) => {
+                setDateRange(update);
+              }}
+              isClearable={true}
+              className="w-full border-none drop-shadow-sm h-9 rounded-md text-sm focus:ring-0"
+            />
+          </div>
+        )}
+      </div>
+      <Controller
+        name="origin"
+        control={control}
+        defaultValue={""}
+        render={({ field }) => {
+          return (
+            <SelectInput
+              {...field}
+              label={t("contacts:filters:origin")}
+              name="origin"
+              options={filteredContactSources}
+              selectedOption={contactSource}
+              onChangeInput={setQuerySource}
+              error={!field.value && errors.origin}
+              setValue={setValue}
+            />
+          );
+        }}
+      />
+      {/* <Controller
 				name="createdby"
 				control={control}
 				defaultValue={[]}
@@ -224,45 +222,45 @@ const FormFilters = () => {
 					/>
 				)}
 			/> */}
-			<SelectDropdown
-				label={t('contacts:filters:created-by')}
-				name="createdby"
-				options={filteredContactCreated}
-				onChangeInput={setQueryCreated}
-				selectedOption={null}
-				setValue={setValue}
-			/>
-			<div className='my-2 flex gap-2 items-center'>
-				<Button
-					type="button"
-					label={t('contacts:filters:add-field')}
-					buttonStyle="text"
-					iconLeft={<PlusIcon className="h-4 w-4 text-gray-60" />}
-				/>
-				<Button
-					type="button"
-					label={t('contacts:filters:restore')}
-					buttonStyle="text"
-					iconLeft={<PlusIcon className="h-4 w-4 text-gray-60" />}
-				/>
-			</div>
-			<div className="flex gap-2 justify-center mt-4">
-				<Button
-					type="submit"
-					label={t('contacts:filters:search')}
-					buttonStyle="primary"
-					className="py-1 px-3"
-					iconLeft={<FaMagnifyingGlass className="h-4 w-4 text-white" />}
-				/>
-				<Button
-					type="button"
-					label={t('contacts:filters:restart')}
-					buttonStyle="secondary"
-					className="py-1 px-3"
-				/>
-			</div>
-		</form>
-	);
+      <SelectDropdown
+        label={t("contacts:filters:created-by")}
+        name="createdby"
+        options={filteredContactCreated}
+        onChangeInput={setQueryCreated}
+        selectedOption={null}
+        setValue={setValue}
+      />
+      <div className="my-2 flex gap-2 items-center">
+        <Button
+          type="button"
+          label={t("contacts:filters:add-field")}
+          buttonStyle="text"
+          iconLeft={<PlusIcon className="h-4 w-4 text-gray-60" />}
+        />
+        <Button
+          type="button"
+          label={t("contacts:filters:restore")}
+          buttonStyle="text"
+          iconLeft={<PlusIcon className="h-4 w-4 text-gray-60" />}
+        />
+      </div>
+      <div className="flex gap-2 justify-center mt-4">
+        <Button
+          type="submit"
+          label={t("contacts:filters:search")}
+          buttonStyle="primary"
+          className="py-1 px-3"
+          iconLeft={<FaMagnifyingGlass className="h-4 w-4 text-white" />}
+        />
+        <Button
+          type="button"
+          label={t("contacts:filters:restart")}
+          buttonStyle="secondary"
+          className="py-1 px-3"
+        />
+      </div>
+    </form>
+  );
 };
 
 export default FormFilters;

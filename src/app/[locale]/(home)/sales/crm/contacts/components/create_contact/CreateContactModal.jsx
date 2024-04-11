de9f8bddfbb1acbe58db21ec78dc78c3f33056e7
yamilerayme <yamilerayme@blindariesgos.com>
2024-04-11 12:35:10 -0400

    fix- elimanr archivos que no se usan

--- src/app/[locale]/(home)/sales/crm/contacts/components/create_contact/CreateContactModal.jsx
+++ src/app/[locale]/(home)/sales/crm/contacts/components/create_contact/CreateContactModal.jsx
@@ -7,7 +7,7 @@ import useAppContext from "@/context/app";
 export default function CreateContactModal() {
   const { openModal, setOpenModal } = useAppContext();
   return (
-    <SlideOver openModal={openModal} setOpenModal={setOpenModal} colorTag="bg-yellow-100" labelTag="contact">
+    <SlideOver openModal={openModal} colorTag="bg-yellow-100" labelTag="contact" samePage={`/sales/crm/contacts`}>
       <CreateContact />
     </SlideOver>
   );

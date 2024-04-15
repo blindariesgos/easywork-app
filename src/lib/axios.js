e28bcd6a177926100521010b47d29bba0ac15e3a
yrayme <45949572+yrayme@users.noreply.github.com>
2024-04-15 14:23:44 -0400

    fix-create contact

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,27 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
 
 const config = axios.defaults;
-config.headers.common = {
-  ...config.headers.common,
-  "Content-Type": "multipart/form-data",
-  "Authorization": "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6W3siaWQiOiIzZmY3ODZkNi04Y2M5LTRkMjktODQxMC0xNzE5N2QyNWVmMGYiLCJuYW1lIjoidXNlciJ9XSwiaXNUd29GYWN0b3JFbmFibGVkIjpmYWxzZSwiaWF0IjoxNzEzMjAzNDk2LCJleHAiOjE3MTMyMDcwOTYsInN1YiI6IjM3ZTNiOTQ3LWI1NmItNDNiMy1hNjE3LTMwODk3ZjRmNTE5MyJ9.X_YmYNND3cH1rer9u_9llsreJ4cGOGm1VUUuXBgw6hM"
+
+const instance = (contentType = "application/json") => {
+  const axiosInstance = axios.create({
+    baseURL: "https://api.easywork.com.mx/v1",
+    headers: {
+      ...config.headers.common,
+      "Content-Type": contentType,
+      Authorization:
+        "Bearer " +
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6W3siaWQiOiIzZmY3ODZkNi04Y2M5LTRkMjktODQxMC0xNzE5N2QyNWVmMGYiLCJuYW1lIjoidXNlciJ9XSwiaXNUd29GYWN0b3JFbmFibGVkIjpmYWxzZSwiaWF0IjoxNzEzMjA1MTEzLCJleHAiOjE3MTMyMDg3MTMsInN1YiI6IjM3ZTNiOTQ3LWI1NmItNDNiMy1hNjE3LTMwODk3ZjRmNTE5MyJ9.G1Qi5PLfCwQulkVdtWcDhgYQ9Ix8VjExbUHPxXT3o3Y",
+    },
+  });
+  // Data Response Interceptor
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      return response.data;
+    },
+    (error) => {
+      // console.log("error axios", Promise.reject(error))
+      // throw new Error(error)
+      return Promise.reject(JSON.stringify(error.response.data));
+    }
+  );
+
+  return axiosInstance;
 };
-const instance = axios.create({
-  baseURL: 'https://api.easywork.com.mx/v1',
-  ...config.headers.common
-});
-
-// Data Response Interceptor
-instance.interceptors.response.use(
-  (response) => {
-    return response.data;
-  },
-  (error) => {
-    // console.log("error axios", Promise.reject(error))
-    // throw new Error(error)
-    return Promise.reject(JSON.stringify(error.response.data));
-  }
-);
-
 
 export default instance;

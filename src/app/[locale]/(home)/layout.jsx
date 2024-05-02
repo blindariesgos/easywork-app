ea258f2fed4275faf99bacd5caae1253eba4d7a1
FariaSta <davidfarias@blindariesgos.com>
2024-05-02 12:24:18 -0400

    arreglos en la estetica

--- src/app/[locale]/(home)/layout.jsx
+++ src/app/[locale]/(home)/layout.jsx
@@ -13,7 +13,7 @@ export default function HomeLayout({ children }) {
 					<div className="w-full h-screen">
 						<div className="flex">
 							<Sidebar />
-							<main className="h-screen overflow-auto w-full p-0.5 sm:p-2 md:p-4">
+							<main className="h-screen overflow-auto w-full p-0.5 sm:p-0 md:p-4">
 								<div className="h-full">{children}</div>
 							</main>
 						</div>

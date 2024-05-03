91eddd13647bf2b6b35e2ec8e35fe6e22ff4af0c
FariaSta <davidfarias@blindariesgos.com>
2024-05-03 15:27:41 -0400

    env

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,7 +5,7 @@ const config = axios.defaults;
 
 const instance = (contentType = "application/json") => {
   const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/v1",
+    baseURL: process.env.API_HOST,
     headers: {
       ...config.headers.common,
       "Content-Type": contentType,

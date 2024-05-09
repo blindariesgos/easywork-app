aa8dfedface468fd9b3e0c12a087c9664465adca
FariaSta <davidfarias@blindariesgos.com>
2024-05-09 18:52:38 -0400

    modal de anadir correo gmail 0Auth

---
+++ src/app/[locale]/(home)/tools/tool/mails/components/ModalAddFolders.jsx
@@ -0,0 +1,80 @@
+import Tag from "@/components//Tag";
+import SliderOverShort from "@/components//SliderOverShort";
+
+export default function ModalAddFolders() {
+  return (
+    <SliderOverShort openModal={false}>
+      <Tag onclick={() => setModalC(false)} className="bg-green-500" />
+      <div className="bg-gray-300 max-md:w-screen w-96 rounded-l-2xl overflow-y-auto h-screen">
+        <div className="m-3 font-medium text-lg">
+          <h1>Configurar Folders</h1>
+        </div>
+        <div className="m-3 py-5 bg-gray-100 rounded-2xl">
+          <div className="bg-white px-2">
+            <div className="p-3">
+              <h1 className="font-medium text-lg">Synchronize folders</h1>
+            </div>
+            <div className="text-xs">
+              <div className="flex ml-2">
+                <input type="checkbox" />
+                <p className="ml-1">Select all</p>
+              </div>
+              <div className="mt-4 ml-4">
+                {folderData.map((data, index) => (
+                  <div className="flex mt-4 ml-2" key={index}>
+                    <input
+                      type="checkbox"
+                      checked={data.state}
+                      onChange={(e) => {
+                        const newFolderData = [...folderData];
+                        newFolderData[index] = {
+                          ...newFolderData[index],
+                          state: e.target.checked,
+                        };
+                        setFolderData(newFolderData);
+                      }}
+                    />
+                    <p className="ml-1">{data.folder}</p>
+                  </div>
+                ))}
+              </div>
+              <div className="m-3 text-xs my-4 w-80">
+                <h1 className="font-medium text-lg border-b-4 border-black pb-1">
+                  Folder rules
+                </h1>
+                <p className="p-2">
+                  Save sent emails to folder{" "}
+                  <span className="text-cyan-500">INBOX / Sent</span>
+                </p>
+                <p className="p-2">
+                  Move deleted emails to folder{" "}
+                  <span className="text-cyan-500">INBOX / Sent</span>
+                </p>
+                <p className="p-2">
+                  Move spam to folder{" "}
+                  <span className="text-cyan-500">INBOX / Sent</span>
+                </p>
+              </div>
+              <div className="flex justify-around">
+                <button
+                  type="button"
+                  className="hover:bg-primaryhover bg-primary text-white font-bold py-2 px-4 rounded-md"
+                  onClick={() => saveFoldersData()}
+                >
+                  Guardar
+                </button>
+                <button
+                  type="button"
+                  className="hover:bg-gray-800 bg-gray-700 text-white font-bold py-2 px-4 rounded-md"
+                  onClick={() => setModalC(false)}
+                >
+                  Cancelar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </SliderOverShort>
+  );
+}
--- src/app/[locale]/(home)/tools/tool/mails/page.jsx
+++ src/app/[locale]/(home)/tools/tool/mails/page.jsx
@@ -1,8 +1,6 @@
 "use client";
-import Tag from "../../../../../../components/Tag";
 import React, { useState } from "react";
 import Image from "next/image";
-import SliderOverShort from "../../../../../../components/SliderOverShort";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import {
@@ -13,12 +11,16 @@ import {
 } from "../../../../../../lib/apis";
 import { getApiError } from "../../../../../../utils/getApiErrors";
 import axios from "axios";
+import ModalAddGmail from "./components/ModalAddGmail";
+import Tag from "@/components/Tag";
+import ModalAddFolders from "./components/ModalAddFolders";
 
 export default function IngresarEmail() {
   const session = useSession();
   const router = useRouter();
   const [modalG, setModalG] = useState(false);
   const [modalC, setModalC] = useState(false);
+  const [gmailState, setGmailState] = useState(false);
 
   const [ImapData, setImapData] = useState({
     host: null,
@@ -106,7 +108,8 @@ export default function IngresarEmail() {
     {
       name: "Gmail",
       src: "/icons/emails/gmail.svg",
-      click: () => toKnowEmail(),
+      click: () => setGmailState(true),
+      // click: () => toKnowEmail(),
     },
     {
       name: "ICloud",
@@ -174,233 +177,9 @@ export default function IngresarEmail() {
           ))}
         </ul>
       </div>
-      <SliderOverShort openModal={modalG}>
-        <Tag onclick={() => setModalG(false)} className="bg-green-500" />
-        <div className="bg-gray-300 rounded-l-2xl max-md:w-screen w-96 overflow-y-auto h-screen">
-          <div className="m-3 font-medium text-lg">
-            <h1>Gestionar buzón</h1>
-          </div>
-          <div className="m-3 p-5 pr-8 bg-gray-100 rounded-2xl">
-            <div>
-              <h1 className="font-medium">IMAP</h1>
-              <h2 className="font-semibold">E-mail</h2>
-              <h3>Armandoalbb@gmail.com</h3>
-              <p className="text-xs">Último revisado Hace 10 minutos Éxito</p>
-            </div>
-            <div className="text-xs">
-              <div className="mt-2">
-                <p className="ml-2">Dirección del servidor IMAP</p>
-                <input
-                  type="text"
-                  className="rounded-md border-0 w-full"
-                  onChange={(e) =>
-                    setImapData((prevState) => ({
-                      ...prevState,
-                      host: e.target.value,
-                    }))
-                  }
-                />
-              </div>
-              <div className="flex mt-3">
-                <div className="w-20">
-                  <p className="ml-2">Puerto</p>
-                  <input
-                    type="number"
-                    className="w-full rounded-md border-0"
-                    onChange={(e) =>
-                      setImapData((prevState) => ({
-                        ...prevState,
-                        port: parseInt(e.target.value),
-                      }))
-                    }
-                  />
-                </div>
-                <div className="flex mt-4 ml-2">
-                  <input
-                    type="checkbox"
-                    onChange={(e) =>
-                      setImapData((prevState) => ({
-                        ...prevState,
-                        tls: e.target.checked,
-                      }))
-                    }
-                  />
-                  <p className="ml-1">Usar SSL</p>
-                </div>
-              </div>
-              <div className="mt-2">
-                <p className="ml-2">Iniciar Sesión</p>
-                <input
-                  type="text"
-                  onChange={(e) =>
-                    setImapData((prevState) => ({
-                      ...prevState,
-                      user: e.target.value,
-                    }))
-                  }
-                  className="rounded-md border-0 w-full"
-                />
-              </div>
-              <div className="mt-2">
-                <p className="ml-2">Contraseña</p>
-                <input
-                  type="password"
-                  onChange={(e) =>
-                    setImapData((prevState) => ({
-                      ...prevState,
-                      password: e.target.value,
-                    }))
-                  }
-                  className="rounded-md border-0 w-full"
-                />
-              </div>
-              <p className="mt-2 underline text-violet-800">
-                Configurar carpetas para la sincronización
-              </p>
-              <div className="mt-2">
-                <p className="ml-2">Nombre del buzón</p>
-                <input
-                  type="text"
-                  onChange={(e) =>
-                    setImapData((prevState) => ({
-                      ...prevState,
-                      mailName: e.target.value,
-                    }))
-                  }
-                />
-              </div>
-              <div className="mt-2">
-                <p className="ml-2">Nombre del remitente</p>
-                <input
-                  type="text"
-                  onChange={(e) =>
-                    setImapData((prevState) => ({
-                      ...prevState,
-                      senderName: e.target.value,
-                    }))
-                  }
-                  className="rounded-md border-0 w-full"
-                />
-              </div>
-              <div className="mt-2">
-                <p className="ml-2">
-                  URL de la interfaz web del servidor de correo electrónico
-                </p>
-                <input
-                  type="text"
-                  value="www.gmail.com"
-                  className="rounded-md border-0 w-full"
-                  disabled
-                />
-              </div>
-              <div className="m-3 text-xs my-4 w-full">
-                <h1 className="font-bold text-lg">Acceso al buzón</h1>
-                <p className="bg-gray-300 p-2">
-                  Dé a los empleados permiso de acceso a este buzón para que
-                  puedan recibir y responder mensajes de correo electrónico. Es
-                  una forma sencilla de crear un entorno de colaboración para su
-                  departamento de ventas o servicio de asistencia.
-                </p>
-              </div>
-              <div className="flex justify-around">
-                <button
-                  type="button"
-                  className="hover:bg-primaryhover bg-primary text-white font-bold py-2 px-4 rounded-md"
-                  onClick={() => saveIMAP()}
-                >
-                  Guardar
-                </button>
-                <button
-                  type="button"
-                  className="hover:bg-gray-800 bg-gray-700 text-white font-bold py-2 px-4 rounded-md"
-                >
-                  Inhabilitar
-                </button>
-                <button
-                  type="button"
-                  className="hover:bg-gray-800 bg-gray-700 text-white font-bold py-2 px-4 rounded-md"
-                  onClick={() => setModalG(false)}
-                >
-                  Cancelar
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </SliderOverShort>
-      <SliderOverShort openModal={modalC}>
-        <Tag onclick={() => setModalC(false)} className="bg-green-500" />
-        <div className="bg-gray-300 max-md:w-screen w-96 rounded-l-2xl overflow-y-auto h-screen">
-          <div className="m-3 font-medium text-lg">
-            <h1>Configurar Folders</h1>
-          </div>
-          <div className="m-3 py-5 bg-gray-100 rounded-2xl">
-            <div className="bg-white px-2">
-              <div className="p-3">
-                <h1 className="font-medium text-lg">Synchronize folders</h1>
-              </div>
-              <div className="text-xs">
-                <div className="flex ml-2">
-                  <input type="checkbox" />
-                  <p className="ml-1">Select all</p>
-                </div>
-                <div className="mt-4 ml-4">
-                  {folderData.map((data, index) => (
-                    <div className="flex mt-4 ml-2" key={index}>
-                      <input
-                        type="checkbox"
-                        checked={data.state}
-                        onChange={(e) => {
-                          const newFolderData = [...folderData];
-                          newFolderData[index] = {
-                            ...newFolderData[index],
-                            state: e.target.checked,
-                          };
-                          setFolderData(newFolderData);
-                        }}
-                      />
-                      <p className="ml-1">{data.folder}</p>
-                    </div>
-                  ))}
-                </div>
-                <div className="m-3 text-xs my-4 w-80">
-                  <h1 className="font-medium text-lg border-b-4 border-black pb-1">
-                    Folder rules
-                  </h1>
-                  <p className="p-2">
-                    Save sent emails to folder{" "}
-                    <span className="text-cyan-500">INBOX / Sent</span>
-                  </p>
-                  <p className="p-2">
-                    Move deleted emails to folder{" "}
-                    <span className="text-cyan-500">INBOX / Sent</span>
-                  </p>
-                  <p className="p-2">
-                    Move spam to folder{" "}
-                    <span className="text-cyan-500">INBOX / Sent</span>
-                  </p>
-                </div>
-                <div className="flex justify-around">
-                  <button
-                    type="button"
-                    className="hover:bg-primaryhover bg-primary text-white font-bold py-2 px-4 rounded-md"
-                    onClick={() => saveFoldersData()}
-                  >
-                    Guardar
-                  </button>
-                  <button
-                    type="button"
-                    className="hover:bg-gray-800 bg-gray-700 text-white font-bold py-2 px-4 rounded-md"
-                    onClick={() => setModalC(false)}
-                  >
-                    Cancelar
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </SliderOverShort>
+      <ModalAddGmail state={gmailState}>
+        <Tag onclick={() => setGmailState(false)} className="bg-green-500" />
+      </ModalAddGmail>
     </div>
   );
 }

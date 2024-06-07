60089fd8e71408e20703ad7d6cc7d24429fd38b3
Israel Gonzalez <criptopreto@gmail.com>
2024-06-06 23:03:02 -0400

    Update

--- src/app/[locale]/auth/components/GetPassword.jsx
+++ src/app/[locale]/auth/components/GetPassword.jsx
@@ -32,25 +32,6 @@ export default function GetPassword() {
         <div className="mb-4">
           <h1>Obtener contraseña</h1>
         </div>
-        {/* Usuario */}
-        <div className="relative text-gray-600 focus-within:text-gray-400">
-          <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-            <button
-              type="submit"
-              className="p-1 focus:outline-none focus:shadow-outline"
-            >
-              <UserIcon className="h-5 w-5 text-easywork-main" />
-            </button>
-          </span>
-          <input
-            style={{ border: "none" }}
-            type="search"
-            name="q"
-            className="py-2 text-sm rounded-md pl-10 focus:text-gray-900 placeholder-slate-600"
-            placeholder="Usuario"
-            autoComplete="off"
-          />
-        </div>
         {/* E-mail */}
         <div className="relative text-gray-600 focus-within:text-gray-400 mt-2">
           <span className="absolute inset-y-0 left-0 flex items-center pl-2">

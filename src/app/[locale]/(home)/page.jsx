1e022c0bec84fb217cb5fce69c86368ed490e970
Israel Gonzalez <criptopreto@gmail.com>
2024-06-10 02:21:11 -0400

    Add notifications

--- src/app/[locale]/(home)/page.jsx
+++ src/app/[locale]/(home)/page.jsx
@@ -1,12 +1,14 @@
 "use client"; // Indicamos que es un Client Component
 import { redirect } from "next/navigation";
 import { useEffect } from "react"; // Importa useEffect
-
 export default function Page() {
+
   useEffect(() => {
     redirect("/home"); // Realiza la redirección cuando el componente se monta
   }, []); // El array de dependencias vacío garantiza que se ejecute una vez
 
+
+
   // No es necesario retornar nada, ya que la redirección se produce antes del renderizado
-  return null;
+  return null
 }

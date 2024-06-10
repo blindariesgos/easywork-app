f1f964f9febfb4f284ef2876d44decaab11805a1
Israel Gonzalez <criptopreto@gmail.com>
2024-06-10 02:29:32 -0400

    Fix

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCds8g01XKql94ARwrc8PRCcWlXpBSP1rQ",
+  authDomain: "easywork-423220.firebaseapp.com",
+  projectId: "easywork-423220",
+  storageBucket: "easywork-423220.appspot.com",
+  messagingSenderId: "252638396991",
+  appId: "1:252638396991:web:b1617760e2a8bbdc22ae8a",
+  measurementId: "G-S80J4NHTYM",
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});

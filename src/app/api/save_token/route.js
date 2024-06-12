951ee14f0ecc4222df0f64001a6bc17f1b5e64c0
Israel Gonzalez <criptopreto@gmail.com>
2024-06-12 03:21:04 -0400

    DEBUG

--- src/app/api/save_token/route.js
+++ src/app/api/save_token/route.js
@@ -1,10 +1,5 @@
-export default async function handler(req, res) {
-  console.log(req.method);
-  if (req.method !== "POST") {
-    return res.status(405).end(); // Method Not Allowed
-  }
-
+export async function POST(req) {
   console.log(req.body);
 
-  res.status(200).json(true);
+  return Response.json(res);
 }

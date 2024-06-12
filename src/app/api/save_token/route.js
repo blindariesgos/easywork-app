d60c7c1fcec7274312ef4f7ff0ca2e8887f29629
Israel Gonzalez <criptopreto@gmail.com>
2024-06-12 04:11:09 -0400

    Debug

--- src/app/api/save_token/route.js
+++ src/app/api/save_token/route.js
@@ -1,8 +1,14 @@
+import axios from "@/src/lib/axios";
+
 export async function POST(req) {
   try {
     const { token } = await req.json();
 
-    console.log(token);
+    const res = await axios().post(`/v1/notify/push/save-token`, {
+      token,
+    });
+
+    console.log(res, token);
 
     return Response.json(true);
   } catch (error) {

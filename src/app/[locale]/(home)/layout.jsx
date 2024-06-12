17856ef6f6d418023bd5526acf268949ea402bbd
Israel Gonzalez <criptopreto@gmail.com>
2024-06-12 01:20:45 -0400

    Update

--- src/app/[locale]/(home)/layout.jsx
+++ src/app/[locale]/(home)/layout.jsx
@@ -13,9 +13,10 @@ export default function HomeLayout({ children }) {
       <AppContextProvider>
         <LoggedInProvider>
           <SocketProvider>
+            <FirebaseMessaging />
+
             <div className="flex h-screen">
               <Sidebar />
-              <FirebaseMessaging />
               <div className="flex flex-col flex-grow w-full p-0.5 sm:p-0 md:p-4 overflow-y-auto">
                 {/* Contenedor principal flexible */}
                 <main className="flex-grow">

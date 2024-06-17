7c6625bd2553401af34c51f1785386e79dfd6f56
davidfarias30 <davidfarias@blindariesgos.com>
2024-06-17 15:02:46 -0400

    menu desplegable

--- src/app/[locale]/(home)/tools/(tool)/drive/components/DriveHeader.jsx
+++ src/app/[locale]/(home)/tools/(tool)/drive/components/DriveHeader.jsx
@@ -1,18 +1,37 @@
 "use client";
-import React from "react";
+import React, {Fragment} from "react";
 import CreateDocumentButton from "./CreateDocument";
 import { Cog8ToothIcon, TrashIcon } from "@heroicons/react/20/solid";
 import DriveBreadcrumb from "./DriveBreadcrumb";
 import { useTranslation } from "react-i18next";
-import { ChevronDownIcon, Bars3Icon, Squares2X2Icon } from "@heroicons/react/16/solid";
+import {
+  ChevronDownIcon,
+  Bars3Icon,
+  Squares2X2Icon,
+} from "@heroicons/react/16/solid";
 import Image from "next/image";
 import useAppContext from "../../../../../../../context/app";
-import FiltersContact from './filters/FiltersContact';
-import { FaMagnifyingGlass } from 'react-icons/fa6';
+import FiltersContact from "./filters/FiltersContact";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { Menu, Transition } from "@headlessui/react";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function DriveHeader() {
   const { t } = useTranslation();
   const { setDriveView } = useAppContext();
+
+  const itemOptions = [
+    { name: t("tools:drive:organizer:by-name") },
+    { name: t("tools:drive:organizer:by-id") },
+    { name: t("tools:drive:organizer:by-size") },
+    { name: t("tools:drive:organizer:by-change-date") },
+    { name: t("tools:drive:organizer:reverse") },
+    { name: t("tools:drive:organizer:mixed-classification") },
+  ];
+
   return (
     <header className="flex flex-col">
       <div className="lg:px-6 px-2 flex gap-3 items-center bg-white py-4 rounded-md">
@@ -20,10 +39,10 @@ export default function DriveHeader() {
           {t("tools:drive:name")}
         </h1>
         <CreateDocumentButton />
-				<div className="flex-grow">
-					<div className="flex border px-1 py-1 bg-gray-300 items-center rounded-md gap-x-2">
-						<FiltersContact />
-						{/* <div className="flex items-center w-full">
+        <div className="flex-grow">
+          <div className="flex border px-1 py-1 bg-gray-300 items-center rounded-md gap-x-2">
+            <FiltersContact />
+            {/* <div className="flex items-center w-full">
 							<FaMagnifyingGlass className="h-4 w-4 text-primary" />
 							<input
 								type="search"
@@ -33,8 +52,8 @@ export default function DriveHeader() {
 								placeholder={t('contacts:header:search')}
 							/>
 						</div> */}
-					</div>
-				</div>
+          </div>
+        </div>
         {/* <div className="flex-grow">
           <label htmlFor="search" className="sr-only">
             {t("tools:drive:search")}
@@ -64,14 +83,57 @@ export default function DriveHeader() {
       <div className="flex-none items-center justify-between border-b border-gray-200 py-4 hidden lg:flex">
         <DriveBreadcrumb />
         <div className="flex mr-3">
-          <div className="flex text-gray-400 border-r-2 pr-2">
-            <p className="text-nowrap">{t("tools:drive:organizer:by-name")}</p>
-            <ChevronDownIcon className="h-6 w-6" />
-          </div>
+          <Menu as="div" className="relative hover:bg-slate-50/30 w-10 md:w-auto py-2 px-1 rounded-lg">
+            <Menu.Button className="-m-1.5 flex items-center p-1.5">
+              <p className="text-nowrap">
+                {t("tools:drive:organizer:by-name")}
+              </p>
+              <ChevronDownIcon className="h-6 w-6" />
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute left-0 z-50 mt-2.5 w-40 rounded-md bg-white py-2 shadow-lg focus:outline-none">
+                {itemOptions.map((item) => (
+                  <Menu.Item key={item.name}>
+                    {({ active }) => (
+                      <div
+                        className={classNames(
+                          active ? "bg-gray-50" : "",
+                          "block px-3 py-1 text-sm leading-6 text-black cursor-pointer"
+                        )}
+                      >
+                        {item.name}
+                      </div>
+                    )}
+                  </Menu.Item>
+                ))}
+              </Menu.Items>
+            </Transition>
+          </Menu>
           <div className="flex text-easywork-main">
-            <Bars3Icon className="h-6 w-6 ml-1" onClick={() => setDriveView("table")} />
-            <Squares2X2Icon className="h-6 w-6 ml-1" onClick={() => setDriveView("icon")} />
-            <Image className="h-6 w-5 ml-1 fill-easywork-main" src="/icons/grid-3x3.svg" alt="big icons" width={18} height={18} onClick={() => setDriveView("thumb")} />
+            <Bars3Icon
+              className="h-6 w-6 ml-1"
+              onClick={() => setDriveView("table")}
+            />
+            <Squares2X2Icon
+              className="h-6 w-6 ml-1"
+              onClick={() => setDriveView("icon")}
+            />
+            <Image
+              className="h-6 w-5 ml-1 fill-easywork-main"
+              src="/icons/grid-3x3.svg"
+              alt="big icons"
+              width={18}
+              height={18}
+              onClick={() => setDriveView("thumb")}
+            />
           </div>
         </div>
       </div>

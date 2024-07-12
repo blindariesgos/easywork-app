414ab1faf768865adea2ca8ba7bad8f1b2c20803
davidfarias30 <davidfarias@blindariesgos.com>
2024-07-12 13:35:51 -0400

    send email

--- src/app/[locale]/(home)/tools/(tool)/webmail/components/SelectDropdown.jsx
+++ src/app/[locale]/(home)/tools/(tool)/webmail/components/SelectDropdown.jsx
@@ -2,8 +2,10 @@
 "use client";
 import { Combobox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import React, { Fragment, useEffect, useState } from "react";
+import { postLead } from "../../../../../../../lib/apis";
 
 function SelectDropdown({
   label,
@@ -15,10 +17,15 @@ function SelectDropdown({
   error,
   setValue,
   className,
+  setContactsArray,
 }) {
+  const session = useSession();
   const registerInput = register && register(name);
   const [selected, setSelected] = useState(selectedOption);
   const [query, setQuery] = useState("");
+  const [contacts, setContacts] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   useEffect(() => {
     if (selectedOption) {
@@ -28,6 +35,10 @@ function SelectDropdown({
 
   useEffect(() => {
     if (selected) setValue && setValue(selected?.email, selected?.id);
+    if (selected) {
+      setContacts((prevContacts) => [...prevContacts, selected?.email]);
+    }
+    setContactsArray(contacts);
   }, [selected, setValue, name]);
 
   const filteredElements =
@@ -38,6 +49,32 @@ function SelectDropdown({
             .toLowerCase()
             .includes(query.toLowerCase());
         });
+
+  const handleRemoveContact = (index) => {
+    const updatedContacts = [...contacts];
+    updatedContacts.splice(index, 1);
+    setContacts(updatedContacts);
+  };
+
+  useEffect(() => {}, []);
+
+  function isEmail(str) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(str);
+  }
+
+  async function okContact() {
+    const data = {
+      name: firstName,
+      lastName,
+      emails_dto: query,
+      sourceId: "4fad10af-24cc-4589-897b-ac56cf576827",
+      assignedById: session.data.user.id,
+      typeId: "e26df605-4029-4a58-8f99-e50df721fa9e",
+    };
+    const response = await postLead(data);
+    console.log(response);
+  }
 
   return (
     <div className={className}>
@@ -51,22 +88,28 @@ function SelectDropdown({
           {label}
         </Combobox.Label>
         <div className="relative mt-1">
-          <div className="">
+          <div className="flex">
+            <div className="bg-white flex items-center px-3 outline-none focus:outline-none focus-visible:outline-none focus-within:outline-none border-none rounded-l-md drop-shadow-sm placeholder:text-xs focus:ring-0 text-sm">
+              {contacts?.length > 0 &&
+                contacts.map((item, index) => (
+                  <div
+                    key={index}
+                    onClick={() => handleRemoveContact(index)}
+                    className="bg-easywork-main text-white p-2 h-6 mr-1 rounded-sm flex items-center cursor-pointer"
+                  >
+                    {item}
+                  </div>
+                ))}
+            </div>
             <Combobox.Input
               // {...registerInput}
-              className="w-full outline-none focus:outline-none focus-visible:outline-none focus-within:outline-none border-none rounded-md drop-shadow-sm placeholder:text-xs focus:ring-0 text-sm"
+              className="w-full outline-none focus:outline-none focus-visible:outline-none focus-within:outline-none border-none rounded-r-md drop-shadow-sm placeholder:text-xs focus:ring-0 text-sm"
               displayValue={(person) => person?.name || person?.username}
               onChange={(event) => {
                 // registerInput && registerInput.onChange(event);
                 setQuery(event.target.value);
               }}
             />
-            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-              <ChevronDownIcon
-                className="h-5 w-5 text-gray-400"
-                aria-hidden="true"
-              />
-            </Combobox.Button>
           </div>
           <Transition
             as={Fragment}
@@ -77,8 +120,32 @@ function SelectDropdown({
           >
             <Combobox.Options className="grid grid-cols-2 absolute z-50 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
               {filteredElements?.length === 0 && query !== "" ? (
-                <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
-                  Nothing found.
+                <div className="flex flex-row items-center justify-center cursor-default select-none px-4 py-2 text-gray-700">
+                  <p>El mensaje será enviado al correo electrónico.</p>
+                  <div className="flex">
+                    <input
+                      type="text"
+                      className="py-2 text-sm rounded-md ml-2 w-full focus:text-gray-900 placeholder-slate-600"
+                      placeholder="Primer nombre"
+                      autoComplete="off"
+                      onChange={(e) => setFirstName(e.target.value)}
+                    />
+                    <input
+                      type="text"
+                      className="ml-3 py-2 text-sm rounded-md w-full focus:text-gray-900 placeholder-slate-600"
+                      placeholder="Apellido"
+                      autoComplete="off"
+                      onChange={(e) => setLastName(e.target.value)}
+                    />
+                  </div>
+                  <div className="ml-3 flex-row items-center justify-center">
+                    <button
+                      className="bg-easywork-main text-white p-3 rounded-md"
+                      onClick={() => okContact()}
+                    >
+                      Ok
+                    </button>
+                  </div>
                 </div>
               ) : (
                 filteredElements &&
@@ -100,6 +167,7 @@ function SelectDropdown({
                           className={`flex items-center gap-2 w-full${
                             selected ? "font-medium" : "font-normal"
                           }`}
+                          onClick={() => setSelected("")}
                         >
                           <div className="w-[20%]">
                             <Image
@@ -123,11 +191,11 @@ function SelectDropdown({
                             >
                               {option.email}
                             </p>
-                            <p
+                            {/* <p
                               className={`text-[10px] text-gray-50 ${active ? "text-white" : "text-black"}`}
                             >
                               {option.phone}
-                            </p>
+                            </p> */}
                           </div>
                         </span>
                         {selected ? (

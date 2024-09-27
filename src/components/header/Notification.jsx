091c8da15f9e4665a5acab516452f94e7c8c74bb
David Farias <davidfarias@blindariesgos.com>
2024-09-27 16:41:00 -0400

    notificaciones icono

--- src/components/header/Notification.jsx
+++ src/components/header/Notification.jsx
@@ -3,7 +3,7 @@ import { BellIcon, FlagIcon, PlayCircleIcon } from "@heroicons/react/20/solid";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-export default function Status() {
+export default function Notification() {
   const { notifications, setIsOpen } = useNotifyContext();
   const { t } = useTranslation();
 
@@ -12,15 +12,13 @@ export default function Status() {
 
   return (
     <div className="md:flex flex-col gap-1 items-start hidden">
-      <div className="flex gap-x-2">
-        <div className="flex items-center gap-x-1">
-          <FlagIcon className="h-3 w-3 text-black" aria-hidden="true" />
-          <p className="text-black text-xs">10</p>
-        </div>
-      </div>
-      <div className="flex items-center gap-1">
-        <PlayCircleIcon className="h-3 w-3 text-black" aria-hidden="true" />
-        <p className="text-xs text-black">{t('common:header:working')}</p>
+      <div className="flex gap-x-1">
+        <button className="flex items-center gap-x-1" onClick={() => {
+          setIsOpen(true);
+        }}>
+          <BellIcon className="h-6 w-6 text-green-500" aria-hidden="true" />
+          <p className="text-black text-xs">{unreadNotifications?.length ?? 0}</p>
+        </button>
       </div>
     </div>
   );

89f362517cc7eeaf4cbca749f8027c1682b0e92e
Yonei Sanchez <yoneisanchez@gmail.com>
2024-10-11 20:45:26 -0400

    Update regularExp.js

--- src/utils/regularExp.js
+++ src/utils/regularExp.js
@@ -1,1 +1,1 @@
-export const VALIDATE_EMAIL_REGEX = /^[a-zA-Z0-9]+([._%+-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,10})+$/
+export const VALIDATE_EMAIL_REGEX = /^[a-zA-Z0-9]+([._%+-][a-zA-Z0-9]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,10})+$/

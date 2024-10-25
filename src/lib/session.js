5458c7fe042ea32dcaec2ea68c1ec3c799f025be
Israel Gonzalez <criptopreto@gmail.com>
2024-10-25 04:28:25 -0400

    Fix error handling in clearSession function

--- src/lib/session.js
+++ src/lib/session.js
@@ -31,6 +31,7 @@ export async function clearSession() {
     const response = await signOut({ redirect: true });
     logger.info("Session cleared:", response);
   } catch (error) {
+    console.log("Error cerrando cesion", error);
     logger.error("Error clearing session:", error);
     throw error; // Relanza el error para manejarlo en otro lugar
   }

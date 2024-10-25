02479e9d0f94330148f801fc17adfeb0f5fa3636
Yonei Sanchez <yoneisanchez@gmail.com>
2024-10-25 11:51:22 -0400

    acciones masivas kanban recibos

--- src/app/[locale]/(home)/control/portafolio/receipts/@kanban/components/Column.jsx
+++ src/app/[locale]/(home)/control/portafolio/receipts/@kanban/components/Column.jsx
@@ -1,9 +1,21 @@
 import Card from "./Card";
-import { Fragment, useEffect, useMemo, useState } from "react";
+import {
+  Fragment,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { useDroppable, DragOverlay } from "@dnd-kit/core";
 import clsx from "clsx";
 import { getContactId, getReceiptKanbanByStateId } from "@/src/lib/apis";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { formatToCurrency } from "@/src/utils/formatters";
+import useReceiptContext from "@/src/context/receipts";
+import { UsersContext } from "@/src/context";
+import useCrmContext from "@/src/context/crm";
 const Column = ({
   id,
   color,
@@ -12,7 +24,6 @@ const Column = ({
   setItemDrag,
   status,
   updateStages,
-  setUpdateStages,
 }) => {
   const { isOver, setNodeRef } = useDroppable({
     id,
@@ -24,26 +35,44 @@ const Column = ({
   const [page, setPage] = useState(0);
   const [hasMore, setHasMore] = useState(true);
   const [totalItems, setTotalItems] = useState();
+  const [totalAmount, setTotalAmount] = useState(0);
+  const { filters } = useReceiptContext();
+  const [checked, setChecked] = useState(false);
+  const [indeterminate, setIndeterminate] = useState(false);
+  const checkbox = useRef();
+  const {
+    selectedContacts: selectedReceipts,
+    setSelectedContacts: setSelectedReceipts,
+  } = useCrmContext();
 
   const getReceipts = async (defaultPage) => {
-    const response = await getReceiptKanbanByStateId({
-      "pagination[0][stageId]": id,
-      "pagination[0][limit]": 10,
-      "pagination[0][page]": (defaultPage ? defaultPage : page) + 1,
-      stageIds: id,
-    });
-    console.log(title, response);
-    const auxItems = [...items, ...response[0].receipts];
+    try {
+      const params = {
+        "pagination[0][stageId]": id,
+        "pagination[0][limit]": 10,
+        "pagination[0][page]": (defaultPage ? defaultPage : page) + 1,
+        stageIds: id,
+        ...filters,
+      };
+      const response = await getReceiptKanbanByStateId(params);
+      console.log(title, response, params);
+      const auxItems =
+        page == 0 || defaultPage == 0
+          ? response[0].receipts
+          : [...items, ...response[0].receipts];
+      setItems(auxItems);
+      if (page == 0 || defaultPage == 0) {
+        setTotalItems(response[0].totalReceipts);
+        setTotalAmount(response[0].totalAmount);
+      }
+      if (auxItems.length >= response[0].totalReceipts) {
+        setHasMore(false);
+      }
 
-    setItems(auxItems);
-    if (page == 0 || defaultPage == 0) {
-      setTotalItems(response[0].totalReceipts);
+      setPage((defaultPage ? defaultPage : page) + 1);
+    } catch (error) {
+      console.log({ error });
     }
-    if (auxItems.length >= response[0].totalReceipts) {
-      setHasMore(false);
-    }
-
-    setPage((defaultPage ? defaultPage : page) + 1);
   };
 
   useEffect(() => {
@@ -52,10 +81,14 @@ const Column = ({
 
   useEffect(() => {
     if (updateStages.includes(id)) {
-      setItems([]);
       getReceipts(0);
     }
   }, [updateStages]);
+
+  useEffect(() => {
+    console.log({ filters });
+    getReceipts(0);
+  }, [filters]);
 
   useEffect(() => {
     if (activeId) {
@@ -64,6 +97,28 @@ const Column = ({
     }
   }, [activeId]);
 
+  const toggleAll = useCallback(() => {
+    setSelectedReceipts(
+      checked || indeterminate
+        ? []
+        : [...selectedReceipts, ...items.map(({ id }) => id)]
+    );
+    setChecked(!checked && !indeterminate);
+    setIndeterminate(false);
+  }, [checked, indeterminate, items, setSelectedReceipts]);
+
+  useLayoutEffect(() => {
+    if (checkbox.current) {
+      const isIndeterminate =
+        selectedReceipts &&
+        selectedReceipts.length > 0 &&
+        !items.every((item) => selectedReceipts.includes(item.id));
+      setChecked(items.every((item) => selectedReceipts.includes(item.id)));
+      setIndeterminate(isIndeterminate);
+      checkbox.current.indeterminate = isIndeterminate;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedReceipts, checkbox]);
   return (
     <div
       ref={setNodeRef}
@@ -72,12 +127,24 @@ const Column = ({
         background: isOver ? "#DCDAF1" : color.secondary,
       }}
     >
-      <p
-        className={`w-full text-white font-semibold px-2 py-3 rounded-md text-sm`}
+      <div
+        className={`w-full text-white font-semibold px-2 py-3 rounded-md text-sm flex justify-between items-center`}
         style={{ background: color.primary }}
       >
-        {title} ({totalItems ?? 0})
-      </p>
+        <p>
+          {title} ({totalItems ?? 0})
+        </p>
+        {items.some((item) => selectedReceipts.includes(item.id)) && (
+          <input
+            type="checkbox"
+            className=" h-4 w-4 rounded border-gray-300 text-primary focus:ring-primary"
+            ref={checkbox}
+            checked={checked}
+            onClick={toggleAll}
+          />
+        )}
+      </div>
+      <p className="text-center py-2">{`$ ${formatToCurrency(totalAmount)}`}</p>
       <InfiniteScroll
         dataLength={items.length}
         next={getReceipts}
@@ -90,7 +157,7 @@ const Column = ({
         //   </p>
         // }
       >
-        <div className={clsx("grid grid-cols-1 gap-2 pt-2")}>
+        <div className={clsx("grid grid-cols-1 gap-2 pb-2")}>
           {items.map((receipt, index) => (
             <Card
               receipt={receipt}
--- src/app/[locale]/(home)/custom-import/contacts/CustomImport.jsx
+++ src/app/[locale]/(home)/custom-import/contacts/CustomImport.jsx
@@ -1,5 +1,29 @@
+"use client";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+
 const CustomImport = () => {
-  return <p>Custom import</p>;
+  const { t } = useTranslation();
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  return (
+    <div className="p-8">
+      <h3 className="font-bold">{t("import:contacts:title")}</h3>
+      <TabGroup selectedIndex={selectedIndex} onChange={setSelectedIndex}>
+        <TabList>
+          <Tab>Tab 1</Tab>
+          <Tab>Tab 2</Tab>
+          <Tab>Tab 3</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>Content 1</TabPanel>
+          <TabPanel>Content 2</TabPanel>
+          <TabPanel>Content 3</TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </div>
+  );
 };
 
 export default CustomImport;

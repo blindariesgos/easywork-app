decde1e86b6d273f8c6a70dc697277797492752f
Yonei Sanchez <yoneisanchez@gmail.com>
2024-10-27 19:32:53 -0400

    Inicio modulo de importaciones

--- src/app/[locale]/(home)/custom-import/contacts/CustomImport.jsx
+++ src/app/[locale]/(home)/custom-import/contacts/CustomImport.jsx
@@ -2,23 +2,43 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
-
+import Configurations from "./components/Configurations";
 const CustomImport = () => {
   const { t } = useTranslation();
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <div className="p-8">
-      <h3 className="font-bold">{t("import:contacts:title")}</h3>
+      <h3 className="font-bold pb-6">{t("import:contacts:title")}</h3>
       <TabGroup selectedIndex={selectedIndex} onChange={setSelectedIndex}>
         <TabList>
-          <Tab>Tab 1</Tab>
-          <Tab>Tab 2</Tab>
-          <Tab>Tab 3</Tab>
+          <Tab className="px-3 py-4 bg-[#EDEDED] data-[selected]:opacity-100 opacity-50 rounded-t-[10px] outline-none focus:outline-none">
+            <p className="text-sm font-bold">
+              {t("import:contacts:tabs:config")}
+            </p>
+          </Tab>
+          <Tab className="px-3 py-4 bg-[#EDEDED] data-[selected]:opacity-100 opacity-50 rounded-t-[10px] outline-none focus:outline-none">
+            <p className="text-sm font-bold">
+              {t("import:contacts:tabs:fields")}
+            </p>
+          </Tab>
+          <Tab className="px-3 py-4 bg-[#EDEDED] data-[selected]:opacity-100 opacity-50 rounded-t-[10px] outline-none focus:outline-none">
+            <p className="text-sm font-bold">
+              {t("import:contacts:tabs:duplicate")}
+            </p>
+          </Tab>
+          <Tab className="px-3 py-4 bg-[#EDEDED] data-[selected]:opacity-100 opacity-50 rounded-t-[10px] outline-none focus:outline-none">
+            <p className="text-sm font-bold">
+              {t("import:contacts:tabs:import")}
+            </p>
+          </Tab>
         </TabList>
-        <TabPanels>
-          <TabPanel>Content 1</TabPanel>
+        <TabPanels className="bg-white">
+          <TabPanel>
+            <Configurations />
+          </TabPanel>
           <TabPanel>Content 2</TabPanel>
+          <TabPanel>Content 3</TabPanel>
           <TabPanel>Content 3</TabPanel>
         </TabPanels>
       </TabGroup>

94cefa6620be1ddef8632677efd0f0cd2d4215ce
David Farias <davidfarias@blindariesgos.com>
2024-11-26 21:52:48 -0400

    status del usuario

--- src/components/header/profile/components/Profile.jsx
+++ src/components/header/profile/components/Profile.jsx
@@ -1,37 +1,29 @@
 "use client";
 import useAppContext from "@/src/context/app";
-import { PencilIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
-import React, { Fragment, useCallback, useEffect, useState } from "react";
+import { PencilIcon } from "@heroicons/react/24/outline";
+import React, { useCallback, useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { toast } from "react-toastify";
 import { useTranslation } from "react-i18next";
 import Button from "@/src/components/form/Button";
 import TextInput from "@/src/components/form/TextInput";
 import { Controller, useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
-import SlideOver from "@/src/components/SlideOver";
 import InputPhone from "@/src/components/form/InputPhone";
-import SelectInput from "@/src/components/form/SelectInput";
-import InputDate from "@/src/components/form/InputDate";
-import { FaCalendarDays } from "react-icons/fa6";
 import { handleApiError } from "@/src/utils/api/errors";
 import {
-  Dialog,
-  DialogPanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
   Transition,
-  TransitionChild,
 } from "@headlessui/react";
-import { updateUser, getUsersGroup } from "@/src/lib/apis";
+import { updateUser, getUsersGroup, updateStatus } from "@/src/lib/apis";
 import { reloadSession } from "@/src/lib/axios";
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
-import clsx from "clsx";
 import ProfileImageInput from "@/src/components/ProfileImageInput";
 import { useRouter, useSearchParams } from "next/navigation";
-import LoaderSpinner from "@/src/components/LoaderSpinner";
 import { useSWRConfig } from "swr";
 import Image from "next/image";
-import Tag from "@/src/components/Tag";
 
 export function Profile({ user, id }) {
   const { lists } = useAppContext();
@@ -53,7 +45,6 @@ export function Profile({ user, id }) {
 
   useEffect(() => {
     if (data?.user) {
-      console.log(data);
       lists?.listContact?.contactTypes.length > 0 &&
         setContactType(
           lists?.listContact?.contactTypes.filter(
@@ -72,6 +63,10 @@ export function Profile({ user, id }) {
       });
     }
   }, [data?.user, lists]);
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
 
   const schema = Yup.object().shape({
     email: Yup.string()
@@ -97,7 +92,6 @@ export function Profile({ user, id }) {
 
   useEffect(() => {
     getUsersGroup(data?.user?.id).then((res) => {
-      console.log(res.groups);
       setGroups(res.groups);
     });
   }, []);
@@ -149,43 +143,6 @@ export function Profile({ user, id }) {
     }
   }, []);
 
-  const handleFilesUpload = (event, drop) => {
-    let uploadedImages = [...files];
-    const fileList = drop ? event.dataTransfer.files : event.target.files;
-
-    if (fileList) {
-      for (let i = 0; i < fileList.length; i++) {
-        const file = fileList[i];
-        if (file.size > 5 * 1024 * 1024) {
-          toast.error(t("common:validations:size", { size: 5 }));
-          return;
-        } else {
-          const reader = new FileReader();
-
-          reader.onload = (e) => {
-            setTimeout(() => {
-              const existFile = uploadedImages.some(
-                (item) => item.name === file.name
-              );
-              if (!existFile) {
-                uploadedImages = [
-                  ...uploadedImages,
-                  {
-                    base64: reader.result,
-                    type: file.type.split("/")[0],
-                    name: file.name,
-                  },
-                ];
-                setFiles(uploadedImages);
-              }
-            }, 500);
-          };
-          reader.readAsDataURL(file);
-        }
-      }
-    }
-  };
-
   const handleFormSubmit = async (dataUser) => {
     const previousData = {
       email: data.user.email,
@@ -220,14 +177,9 @@ export function Profile({ user, id }) {
         formData.append(key, body[key]?.toString() || ""); // Convierte los demás valores a string
       }
     }
-
-    for (let pair of formData.entries()) {
-      console.log(pair[0] + ": " + pair[1]);
-    }
     try {
       setLoading(true);
-      const response = await updateUser(data?.user?.id, formData);
-      console.log(response);
+      await updateUser(data?.user?.id, formData);
       await reloadSession();
       update();
       setLoading(false);
@@ -238,171 +190,228 @@ export function Profile({ user, id }) {
     }
   };
 
+  const changeStatus = async (item) => {
+    try {
+      await updateStatus(item);
+      await reloadSession();
+      update();
+    } catch (error) {
+      toast.error("Error al cambiar status");
+    }
+  };
+
+  const status = [
+    {
+      label: t("common:header:status:working"),
+      value: "working",
+    },
+    {
+      label: t("common:header:status:do_not_disturb"),
+      value: "do_not_disturb",
+    },
+    {
+      label: t("common:header:status:on_vacation"),
+      value: "on_vacation",
+    },
+    {
+      label: t("common:header:status:out_of_office"),
+      value: "out_of_office",
+    },
+  ];
+
   // Calculate the user 18th birthday
   const eighteenYearsAgo = new Date();
   eighteenYearsAgo.setFullYear(eighteenYearsAgo.getFullYear() - 18);
   return (
-        <form
-          onSubmit={handleSubmit(handleFormSubmit)}
-          className="grid grid-cols-1 md:grid-cols-2 gap-4 rounded-lg w-full h-[calc(100vh_-_160px)]"
-        >
-          {/* Menu Izquierda */}
-          <div className="grid grid-cols-1 gap-4">
-            <div className="rounded-lg bg-white">
-              <div className="flex w-full justify-between pt-4">
-                <div className="px-2 flex items-center bg-easywork-main hover:bg-easywork-mainhover text-white">
-                  {data?.user.roles[0].name}
-                  <ChevronDownIcon className="w-4 h-4" />
-                </div>
-                <p className="py-1 px-2">No molestar</p>
-              </div>
-              <div className="flex flex-col text-sm justify-center items-center w-full h-full">
-                {isEdit ? (
-                  <ProfileImageInput
-                    selectedProfileImage={selectedProfileImage}
-                    onChange={handleProfileImageChange}
-                    disabled={!isEdit}
-                  />
-                ) : (
-                  <div className="p-2">
-                    <Image
-                      width={1080}
-                      height={1080}
-                      src={data?.user?.avatar || "/img/avatar.svg"}
-                      alt="Profile picture"
-                      className="h-60 w-60 flex-none rounded-full text-white fill-white bg-zinc-200 object-cover items-center justify-center"
-                      objectFit="fill"
-                    />
-                  </div>
-                )}
-              </div>
+    <form
+      onSubmit={handleSubmit(handleFormSubmit)}
+      className="grid grid-cols-1 md:grid-cols-2 gap-4 rounded-lg w-full h-[calc(100vh_-_160px)]"
+    >
+      {/* Menu Izquierda */}
+      <div className="grid grid-cols-1 gap-4">
+        <div className="rounded-lg bg-white">
+          <div className="flex w-full justify-between pt-4">
+            <div className="px-2 flex items-center bg-easywork-main hover:bg-easywork-mainhover text-white">
+              {data?.user.roles[0].name}
             </div>
-          </div>
-          {/* Menu Derecha */}
-          <div className="h-auto rounded-lg">
-            <div className="grid grid-cols-1 gap-x-6 bg-white rounded-lg w-full gap-y-3 px-5 pb-9">
-              <div className="flex justify-between py-4 px-2 rounded-md">
-                <h1 className="text-primary font-bold text-2xl">
-                  Información del usuario
-                </h1>
-                {data?.user && (
-                  <button
-                    type="button"
-                    onClick={() => setIsEdit(!isEdit)}
-                    title="Editar"
-                  >
-                    <PencilIcon className="h-6 w-6 text-primary" />
-                  </button>
-                )}
-              </div>
-              <TextInput
-                type="text"
-                label={t("users:form:firstname")}
-                placeholder={t("contacts:create:placeholder-name")}
-                error={errors.firstName && errors.firstName.message}
-                register={register}
-                name="firstName"
-                disabled={!isEdit}
-              />
-              <TextInput
-                type="text"
-                label={t("users:form:lastname")}
-                placeholder={t("contacts:create:placeholder-name")}
-                error={errors.lastName && errors.lastName.message}
-                register={register}
-                name="lastName"
-                disabled={!isEdit}
-              />
-              <TextInput
-                label={t("contacts:create:email")}
-                placeholder={t("contacts:create:placeholder-lastname")}
-                error={errors.email}
-                register={register}
-                name="email"
-                disabled={!isEdit}
-              />
-              <Controller
-                render={({ field: { ref, ...field } }) => {
-                  return (
-                    <InputPhone
-                      name="phone"
-                      field={field}
-                      error={errors.phone}
-                      label={t("contacts:create:phone")}
-                      defaultValue={field.value}
-                      disabled={!isEdit}
-                    />
-                  );
-                }}
-                name="phone"
-                control={control}
-                defaultValue=""
-              />
-              <TextInput
-                label={t("contacts:create:cua")}
-                error={errors.cua}
-                register={register}
-                name="cua"
-                disabled={!isEdit}
-                //value={watch('cua')}
-                // placeholder={t('contacts:create:placeholder-address')}
-              />
-            </div>
-            {groups?.map((group, index) => (
-              <div className="w-full p-1 mt-4 rounded-lg h-60 bg-white overflow-y-auto" key={index}>
-                <h1 className="text-easywork-main p-2 w-full mt-2 font-medium">
-                  Compañía: {group.name}
-                </h1>
-                {group?.users?.map((user, index) => (
-                  <div className="px-3 py-2 text-sm" key={index}>
-                    <div className="mb-3">
-                      <p className="text-gray-50">Miembro del equipo</p>
-                      <div className="flex">
-                        <Image
-                          className="h-12 w-12 rounded-full object-cover"
-                          width={100}
-                          height={100}
-                          src={user?.avatar}
-                          alt="user"
-                        />
-                        <div className="ml-2">
-                          <p>Nombre</p>
-                          <h1 className="font-semibold">
-                            {`${user?.profile?.firstName} ${user?.profile?.lastName}`}
-                          </h1>
-                        </div>
+            <Menu
+              as="div"
+              className="relative hover:bg-slate-50/30 w-10 md:w-auto py-2 pr-4 rounded-lg"
+            >
+              <MenuButton className="flex items-center">
+                <p className="py-1 px-2">{status.find(item => item.value === data?.user?.status)?.label}</p>
+              </MenuButton>
+              <MenuItems
+                transition
+                anchor="bottom end"
+                className=" z-50 mt-2.5 w-32 rounded-md bg-white py-2 shadow-lg focus:outline-none"
+              >
+                {status.map((item) => (
+                  <MenuItem key={item.value}>
+                    {({ active }) => (
+                      <div
+                        onClick={() => changeStatus(item.value)}
+                        className={classNames(
+                          active ? "bg-gray-50" : "",
+                          "block px-3 py-1 text-sm leading-6 text-black cursor-pointer text-end"
+                        )}
+                      >
+                        {item.label}
                       </div>
+                    )}
+                  </MenuItem>
+                ))}
+              </MenuItems>
+            </Menu>
+          </div>
+          <div className="flex flex-col text-sm justify-center items-center w-full h-full">
+            {isEdit ? (
+              <ProfileImageInput
+                selectedProfileImage={selectedProfileImage}
+                onChange={handleProfileImageChange}
+                disabled={!isEdit}
+              />
+            ) : (
+              <div className="p-2">
+                <Image
+                  width={1080}
+                  height={1080}
+                  src={data?.user?.avatar || "/img/avatar.svg"}
+                  alt="Profile picture"
+                  className="h-60 w-60 flex-none rounded-full text-white fill-white bg-zinc-200 object-cover items-center justify-center"
+                  objectFit="fill"
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* Menu Derecha */}
+      <div className="h-auto rounded-lg">
+        <div className="grid grid-cols-1 gap-x-6 bg-white rounded-lg w-full gap-y-3 px-5 pb-9">
+          <div className="flex justify-between py-4 px-2 rounded-md">
+            <h1 className="text-primary font-bold text-2xl">
+              Información del usuario
+            </h1>
+            {data?.user && (
+              <button
+                type="button"
+                onClick={() => setIsEdit(!isEdit)}
+                title="Editar"
+              >
+                <PencilIcon className="h-6 w-6 text-primary" />
+              </button>
+            )}
+          </div>
+          <TextInput
+            type="text"
+            label={t("users:form:firstname")}
+            placeholder={t("contacts:create:placeholder-name")}
+            error={errors.firstName && errors.firstName.message}
+            register={register}
+            name="firstName"
+            disabled={!isEdit}
+          />
+          <TextInput
+            type="text"
+            label={t("users:form:lastname")}
+            placeholder={t("contacts:create:placeholder-name")}
+            error={errors.lastName && errors.lastName.message}
+            register={register}
+            name="lastName"
+            disabled={!isEdit}
+          />
+          <TextInput
+            label={t("contacts:create:email")}
+            placeholder={t("contacts:create:placeholder-lastname")}
+            error={errors.email}
+            register={register}
+            name="email"
+            disabled={!isEdit}
+          />
+          <Controller
+            render={({ field: { ref, ...field } }) => {
+              return (
+                <InputPhone
+                  name="phone"
+                  field={field}
+                  error={errors.phone}
+                  label={t("contacts:create:phone")}
+                  defaultValue={field.value}
+                  disabled={!isEdit}
+                />
+              );
+            }}
+            name="phone"
+            control={control}
+            defaultValue=""
+          />
+          <TextInput
+            label={t("contacts:create:cua")}
+            error={errors.cua}
+            register={register}
+            name="cua"
+            disabled={!isEdit}
+            //value={watch('cua')}
+            // placeholder={t('contacts:create:placeholder-address')}
+          />
+        </div>
+        {groups?.map((group, index) => (
+          <div
+            className="w-full p-1 mt-4 rounded-lg h-60 bg-white overflow-y-auto"
+            key={index}
+          >
+            <h1 className="text-easywork-main p-2 w-full mt-2 font-medium">
+              Compañía: {group.name}
+            </h1>
+            {group?.users?.map((user, index) => (
+              <div className="px-3 py-2 text-sm" key={index}>
+                <div className="mb-3">
+                  <p className="text-gray-50">Miembro del equipo</p>
+                  <div className="flex">
+                    <Image
+                      className="h-12 w-12 rounded-full object-cover"
+                      width={100}
+                      height={100}
+                      src={user?.avatar}
+                      alt="user"
+                    />
+                    <div className="ml-2">
+                      <p>Nombre</p>
+                      <h1 className="font-semibold">
+                        {`${user?.profile?.firstName} ${user?.profile?.lastName}`}
+                      </h1>
                     </div>
                   </div>
-                ))}
+                </div>
               </div>
             ))}
           </div>
-          {/* Botones de acción */}
-          {isEdit && (
-            <div className="flex w-full justify-center px-4 py-4 gap-4 fixed -bottom-4 md:bottom-0 bg-white shadow-[0px_-2px_6px_4px_#00000017]">
-              <Button
-                type="submit"
-                label={
-                  loading
-                    ? t("common:buttons:saving")
-                    : t("common:buttons:save")
-                }
-                disabled={loading}
-                buttonStyle="primary"
-                className="px-3 py-2"
-                // onclick={() => handleSubmit(handleFormSubmit)}
-              />
-              <Button
-                type="button"
-                label={t("common:buttons:cancel")}
-                disabled={loading}
-                buttonStyle="secondary"
-                onclick={() => setIsEdit(false)}
-                className="px-3 py-2"
-              />
-            </div>
-          )}
-        </form>
+        ))}
+      </div>
+      {/* Botones de acción */}
+      {isEdit && (
+        <div className="flex w-full justify-center px-4 py-4 gap-4 fixed -bottom-4 md:bottom-0 bg-white shadow-[0px_-2px_6px_4px_#00000017]">
+          <Button
+            type="submit"
+            label={
+              loading ? t("common:buttons:saving") : t("common:buttons:save")
+            }
+            disabled={loading}
+            buttonStyle="primary"
+            className="px-3 py-2"
+            // onclick={() => handleSubmit(handleFormSubmit)}
+          />
+          <Button
+            type="button"
+            label={t("common:buttons:cancel")}
+            disabled={loading}
+            buttonStyle="secondary"
+            onclick={() => setIsEdit(false)}
+            className="px-3 py-2"
+          />
+        </div>
+      )}
+    </form>
   );
 }

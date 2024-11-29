be6e5b904daa92752bc5cc63146e3b9af98d5b0e
davidfarias30 <165038111+davidfarias30@users.noreply.github.com>
2024-11-29 12:22:48 -0400

    Df/fariasta (#169)
    * status en el header
    
    * agregados
    
    * estado del perfil

--- src/components/header/Status.jsx
+++ src/components/header/Status.jsx
@@ -8,19 +8,21 @@ export default function Status({ status }) {
   const { t } = useTranslation();
 
   // Filtrar las notificaciones leidas
-  const unreadNotifications = notifications?.filter((notification) => !notification.readAt);
+  const unreadNotifications = notifications?.filter(
+    (notification) => !notification.readAt
+  );
 
   return (
     <div className="md:flex flex-col gap-1 items-start hidden">
       <div className="flex gap-x-2">
         <div className="flex items-center gap-x-1">
-          <FlagIcon className="h-3 w-3 text-black" aria-hidden="true" />
+          <FlagIcon className="h-3 w-3 ml-0.5 text-black" aria-hidden="true" />
           <p className="text-black text-xs">0</p>
         </div>
       </div>
-      <div className="flex items-center gap-1">
-        <PlayCircleIcon className="h-3 w-3 text-black" aria-hidden="true" />
-        <p className="text-xs text-black whitespace-nowrap">{status}</p>
+      <div className="flex items-center">
+        {status.icon}
+        <p className="text-xs text-black whitespace-nowrap">{status.label}</p>
       </div>
     </div>
   );

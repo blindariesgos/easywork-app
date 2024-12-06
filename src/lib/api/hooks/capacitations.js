a7363554d1dda2153a0dc244219b4477d873f684
Danniel Sequera <subscriptions@dsequera.com>
2024-12-06 16:06:10 -0400

    Construyendo interfaz de evaluaciones y cursos

--- src/lib/api/hooks/capacitations.js
+++ src/lib/api/hooks/capacitations.js
@@ -1,6 +1,6 @@
 'use client';
 import useSWR from 'swr';
-// import fetcher from '../fetcher';
+import fetcher from '../fetcher';
 import axios from 'axios';
 
 const getQueries = (filters, userId) => {

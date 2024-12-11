c0a97700e099ca1316248ac25cfe6349e5200da4
yoneisanchez3037 <ysanchez@blindariesgos.com>
2024-12-10 20:39:48 -0400

    Ys/10781 (#189)
    * Caraga de prospectos
    
    * agregar cambios
    
    * Update Footer.jsx
    
    * Corregir check list
    
    * cambiar nombre de elementos
    
    * control de duplicidad
    
    * Update TabComment.jsx
    
    * Update KanbanTasks.jsx
    
    * Update formatters.js
    
    * Implementar carga de polizas desde gestion
    
    * implementar carga de renovaciones
    
    * Update TabComment.jsx
    
    * cambiar favicon
    
    * nuevo loader
    
    * vistas listas de juntas
    
    * edicion de reembolsos
    
    * mostrar error de no tener permiso
    
    * modificacion de kanban de prospectos
    
    * kanban polizas
    
    * Update ButtonDiscardedPolicy.jsx
    
    * agregar crm de renovacion
    
    * agregar archivos a los comentarios de las tareas
    
    * implementar adjunto en comentario de tarea
    
    * Agregar vista de agentes
    
    * Update InputCurrency.jsx
    
    * implementar vista de agente
    
    * ramo y caratula
    
    * cambio de fromulario de gestion
    
    * agregar campo de momnto en tabla de prospecto
    
    * agregar filtro de moneda
    
    * agregar modal de agregar poliza
    
    * encabezados de vistas de juntas
    
    * reclutamiento
    
    * carga de renovaciones
    
    * carga renovacion ey erore de kanban tareas y kanban reclutanmiento
    
    ---------
    
    Co-authored-by: Yonei Sanchez <yoneisanchez@gmail.com>
    Co-authored-by: yoneisanchez3037 <yoneisanchez@blindariesgos.com>

--- src/app/[locale]/(home)/agents-management/recruitment/layout.jsx
+++ src/app/[locale]/(home)/agents-management/recruitment/layout.jsx
@@ -1,18 +1,13 @@
 import React, { Suspense } from "react";
 import LayoutPage from "./LayoutPage";
-import LoaderSpinner from "@/src/components/LoaderSpinner";
 import AccompanimentsContextProvider from "@/src/context/accompaniments/provider";
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 
 export default async function Layout({ children, table, kanban }) {
   return (
     <div className="bg-gray-100 h-full p-2 rounded-xl relative">
       <AccompanimentsContextProvider>
-        <LayoutPage>
-          <Suspense fallback={<LoaderSpinner />}>
-            {table}
-            {children}
-          </Suspense>
+        <LayoutPage table={table} kanban={kanban}>
+          {children}
         </LayoutPage>
       </AccompanimentsContextProvider>
     </div>

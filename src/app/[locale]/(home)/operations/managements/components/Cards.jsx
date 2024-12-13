8ace1b1e4a055f500b5a6e3dbc30c99d68511556
Yonei Sanchez <yoneisanchez@gmail.com>
2024-12-13 07:40:22 -0400

    Update Cards.jsx
    cambiar vista de poliza

--- src/app/[locale]/(home)/operations/managements/components/Cards.jsx
+++ src/app/[locale]/(home)/operations/managements/components/Cards.jsx
@@ -2,49 +2,50 @@
 import React, { Suspense, useState, Fragment } from "react";
 import { useTranslation } from "react-i18next";
 import { Radio, RadioGroup } from "@headlessui/react";
+import Button from "@/src/components/form/Button";
 
 const Cards = () => {
   const { t } = useTranslation();
   const cards = [
     {
-      name: `${t("operations:policies:title")} nuevas`,
+      name: `${t("operations:policies:title")}`,
       id: 2,
-      color: "#FFEB04",
+      color: "#e4e7ea",
       amount: "$0,00",
       value: "0",
     },
     {
       name: t("operations:programations:title"),
       id: 7,
-      color: "#0F8BBF",
+      color: "#FFEB04",
       amount: "$0,00",
       value: "0",
     },
     {
-      name: `${t("operations:refunds:title")} nuevos`,
+      name: `${t("operations:refunds:title")}`,
       id: 1,
-      color: "#6b6c6d",
+      color: "#86BEDF",
       amount: "$0,00",
       value: "0",
     },
     {
       name: t("operations:renovations:title"),
       id: 4,
-      color: "#A9EA44",
+      color: "#ACEA4B",
       amount: "$0,00",
       value: "0",
     },
     {
       name: t("operations:fundrecovery:title"),
       id: 6,
-      color: "#AF8764",
+      color: "#8D9194",
       amount: "$0,00",
       value: "0",
     },
     {
-      name: `${t("operations:claims:title")} nuevos`,
+      name: `${t("operations:claims:title")}`,
       id: 3,
-      color: "#86BEDF",
+      color: "#58AED2",
       amount: "$0,00",
       value: "0",
     },
@@ -68,13 +69,19 @@ const Cards = () => {
             className={`absolute w-full h-[7px] top-0 left-0`}
             style={{ background: card.color }}
           />
-          <p className="text-sm">{card.name}</p>
           <div className="w-full">
-            <p className="text-4xl text-right">{card.value}</p>
-            <p className="text-sm text-gray-50 text-right">USD 0,00</p>
-            <p className="text-sm text-gray-50 text-right">$ 0,00</p>
-            <p className="text-sm text-gray-50 text-right">UDIS 0,00</p>
-            {/* <p className="text-sm text-right">{card.amount}</p> */}
+            <p className="text-sm pb-2">{card.name}</p>
+            <p className="text-sm text-gray-50 text-right">USD 0,00 | 0</p>
+            <p className="text-sm text-gray-50 text-right">$ 0,00 | 0</p>
+            <p className="text-sm text-gray-50 text-right">UDIS 0,00 | 0</p>
+            <div className="pt-2 flex justify-between items-center">
+              <Button
+                buttonStyle="primary"
+                label="Ver Todos"
+                className="px-3 py-2"
+              />
+              <p className="text-4xl text-right">{card.value}</p>
+            </div>
           </div>
         </Radio>
       ))}

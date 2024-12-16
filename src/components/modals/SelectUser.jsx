182bcddf966a4477e59b0f7487a908e3d09517be
Yonei Sanchez <yoneisanchez@gmail.com>
2024-12-16 13:50:45 -0400

    correccion de error

--- src/components/modals/SelectUser.jsx
+++ src/components/modals/SelectUser.jsx
@@ -1,6 +1,8 @@
 "use client";
 import clsx from "clsx";
-
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 import React, {
   useEffect,
   useLayoutEffect,
@@ -20,6 +22,8 @@ import {
   DialogTitle,
 } from "@headlessui/react";
 import SelectDropdown from "../form/SelectDropdown";
+import { useTranslation } from "react-i18next";
+import useAppContext from "@/src/context/app";
 
 export default function SelectUserModal({
   isOpen,
@@ -28,8 +32,10 @@ export default function SelectUserModal({
   loading,
   title,
 }) {
+  const { t } = useTranslation();
+  const { lists } = useAppContext();
   const schema = Yup.object().shape({
-    recruitmentManagerId: Yup.string(),
+    developmentManager: Yup.string(),
   });
 
   const {
@@ -43,33 +49,29 @@ export default function SelectUserModal({
   });
 
   const handleFormSubmit = async (data) => {
-    handleClick(data?.recruitmentManagerId);
+    console.log("Lpaaaaaaaaaaaaa");
+    handleClick(data?.developmentManager);
   };
 
   return (
-    <form onSubmit={handleSubmit(handleFormSubmit)}>
-      <Dialog
-        open={isOpen}
-        onClose={() => setIsOpen()}
-        className="relative z-50"
-      >
-        {/* The backdrop, rendered as a fixed sibling to the panel container */}
-        <DialogBackdrop className="fixed inset-0 bg-black/30" />
+    <Dialog open={isOpen} onClose={() => setIsOpen()} className="relative z-50">
+      {/* The backdrop, rendered as a fixed sibling to the panel container */}
+      <DialogBackdrop className="fixed inset-0 bg-black/30" />
 
-        {/* Full-screen container to center the panel */}
-        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-          {/* The actual dialog panel  */}
-          <DialogPanel className="max-w-lg space-y-8 bg-white p-12 rounded-xl">
+      {/* Full-screen container to center the panel */}
+      <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+        {/* The actual dialog panel  */}
+        <DialogPanel className="max-w-lg space-y-8 bg-white p-12 rounded-xl">
+          <form onSubmit={handleSubmit(handleFormSubmit)}>
             <DialogTitle className="font-bold text-xl text-center">
               {title}
             </DialogTitle>
             <div className="p-4">
               <SelectDropdown
                 label={t("agentsmanagement:accompaniments:agent:manager")}
-                name="recruitmentManagerId"
+                name="developmentManager"
                 options={lists?.users ?? []}
-                disabled={!isEdit}
-                error={errors?.recruitmentManagerId}
+                error={errors?.developmentManager}
                 setValue={setValue}
                 watch={watch}
               />
@@ -78,19 +80,19 @@ export default function SelectUserModal({
               <Button
                 label="Cancelar"
                 buttonStyle="secondary"
-                className="px-4 py-2 text-xl"
+                className="px-4 py-2"
                 onclick={() => setIsOpen()}
               />
               <Button
                 label={"Asignar"}
                 buttonStyle="primary"
-                className="px-4 py-2 text-xl"
+                className="px-4 py-2 "
                 type="submit"
               />
             </div>
-          </DialogPanel>
-        </div>
-      </Dialog>
-    </form>
+          </form>
+        </DialogPanel>
+      </div>
+    </Dialog>
   );
 }

38ecf2f4d0043b54d005ac75c25de55fbce5ab95
yoneisanchez3037 <ysanchez@blindariesgos.com>
2024-12-30 18:44:49 -0400

    Ys/10781 (#222)
    * Caraga de prospectos
    
    * agregar cambios
    
    * Update Footer.jsx
    
    * Corregir check list
    
    * cambiar nombre de elementos
    
    * control de duplicidad
    
    * Update TabComment.jsx
    
    * Update KanbanTasks.jsx
    
    * Update formatters.js
    
    * Implementar carga de polizas desde gestion
    
    * implementar carga de renovaciones
    
    * Update TabComment.jsx
    
    * cambiar favicon
    
    * nuevo loader
    
    * vistas listas de juntas
    
    * edicion de reembolsos
    
    * mostrar error de no tener permiso
    
    * modificacion de kanban de prospectos
    
    * kanban polizas
    
    * Update ButtonDiscardedPolicy.jsx
    
    * agregar crm de renovacion
    
    * agregar archivos a los comentarios de las tareas
    
    * implementar adjunto en comentario de tarea
    
    * Agregar vista de agentes
    
    * Update InputCurrency.jsx
    
    * implementar vista de agente
    
    * ramo y caratula
    
    * cambio de fromulario de gestion
    
    * agregar campo de momnto en tabla de prospecto
    
    * agregar filtro de moneda
    
    * agregar modal de agregar poliza
    
    * encabezados de vistas de juntas
    
    * reclutamiento
    
    * carga de renovaciones
    
    * carga renovacion ey erore de kanban tareas y kanban reclutanmiento
    
    * agregar modales para creaccion de meet
    
    * edicion codigo de cliente
    
    * cambiar logica de carga de archivos
    
    * agregar carga de poliza dese contacto
    
    * Update Cards.jsx
    
    cambiar vista de poliza
    
    * cambios varios
    
    * acciones masivas agentes
    
    * asignar manager
    
    * correccion de error
    
    * Corregir token de session para validar cerrado de session
    
    * cambiar visualizacion de poliza cargada
    
    * Corregido bug de subida de achivos en tareas nuevas
    
    * Implementar logica de juntas
    
    * redireccion de tareas desde juntas
    
    * corregido
    
    * cambiar estatus de accion de atrea
    
    * corregir oreja suelta y nombre codigo cliente por operadorara
    
    * conexiones
    
    * cambios varios
    
    * mejoras en agentes
    
    * cambio en conexiones
    
    * kanban
    
    * correcciones en subida de archivos en comentarios
    
    * cambiar filtro el crm filter
    
    * cambios en subida de archivos
    
    ---------
    
    Co-authored-by: Yonei Sanchez <yoneisanchez@gmail.com>
    Co-authored-by: yoneisanchez3037 <yoneisanchez@blindariesgos.com>

--- src/app/[locale]/(home)/agents-management/meetings-and-sessions/meet/MeetEditor.jsx
+++ src/app/[locale]/(home)/agents-management/meetings-and-sessions/meet/MeetEditor.jsx
@@ -1,6 +1,5 @@
 "use client";
-import { Cog8ToothIcon, FireIcon } from "@heroicons/react/20/solid";
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import { Cog8ToothIcon } from "@heroicons/react/20/solid";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import useAppContext from "@/src/context/app";
@@ -10,19 +9,14 @@ import * as yup from "yup";
 import { toast } from "react-toastify";
 import MultipleSelect from "@/src/components/form/MultipleSelect";
 import CRMMultipleSelectV2 from "@/src/components/form/CRMMultipleSelectV2";
-import SubTaskSelect from "@/src/components/form/SubTaskSelect";
 import InputDateV2 from "@/src/components/form/InputDateV2";
 import { FaCalendarDays } from "react-icons/fa6";
-import CheckBoxMultiple from "@/src/components/form/CkeckBoxMultiple";
-import InputCheckBox from "@/src/components/form/InputCheckBox";
 import Button from "@/src/components/form/Button";
 import { useRouter, useSearchParams } from "next/navigation";
 import OptionsTask from "./components/OptionsTask";
 import { useSession } from "next-auth/react";
 import {
   getContactId,
-  postTask,
-  putTaskId,
   getLeadById,
   getPolicyById,
   getReceiptById,
@@ -80,10 +74,10 @@ export default function MeetEditor({ edit, copy, type }) {
       title: edit?.title ?? "",
       startTime: edit?.startTime ?? "",
       participants: edit?.agents ?? [],
-      responsible: edit?.developmentManager.id ?? "",
+      responsible: edit?.developmentManager?.id ?? "",
       observers: edit?.observers ?? [],
       crm: formatCrmData(edit?.crm ?? copy?.crm ?? []),
-      createdBy: edit ? [edit.createdBy] : [],
+      createdBy: edit ? [edit?.createdBy] : [],
       type: type,
     },
     resolver: yupResolver(schemaInputs),
@@ -431,34 +425,24 @@ export default function MeetEditor({ edit, copy, type }) {
           </div>
           <div className={`flex gap-4 flex-wrap mt-4 ${edit && "mb-4"}`}>
             <Button
-              label={
-                loading
-                  ? t("common:saving")
-                  : edit
-                    ? t("tools:tasks:new:update-task")
-                    : t(`agentsmanagement:meetings-and-sessions:${type}:add`)
-              }
-              buttonStyle="primary"
-              disabled={loading}
-              className="px-3 py-2 drop-shadow-lg"
-              onclick={handleSubmit((data) => createMeet(data, false))}
-            />
-            {/* {!edit && (
-              <Button
-                label={t("tools:tasks:new:add-create")}
-                buttonStyle="secondary"
-                disabled={loading}
-                className="px-3 py-2 drop-shadow-lg"
-                onclick={handleSubmit((data) => createTask(data, true))}
-              />
-            )} */}
-
-            <Button
               label={t("common:buttons:cancel")}
               buttonStyle="secondary"
               disabled={loading}
               className="px-3 py-2 drop-shadow-lg"
               onclick={handleCancel}
+            />
+            <Button
+              label={
+                loading
+                  ? t("common:saving")
+                  : edit
+                    ? t("tools:tasks:new:update-task")
+                    : "Agregar junta"
+              }
+              buttonStyle="primary"
+              disabled={loading}
+              className="px-3 py-2 drop-shadow-lg"
+              onclick={handleSubmit((data) => createMeet(data, false))}
             />
           </div>
         </div>

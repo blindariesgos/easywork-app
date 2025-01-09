6ead1912afef916908063d8b8a72913fa8922940
Yonei Sanchez <yoneisanchez@gmail.com>
2025-01-09 18:30:35 -0400

    cambios visuales en notificaciones

--- src/components/notifier/drawer/NotifyList.jsx
+++ src/components/notifier/drawer/NotifyList.jsx
@@ -1,7 +1,9 @@
 "use client";
 import { useNotifyContext } from "@/src/context/notify";
 import { formatDate } from "@/src/utils/getFormatDate";
+import clsx from "clsx";
 import parse from "html-react-parser";
+import moment from "moment";
 import { useEffect, useRef } from "react";
 
 export default function NotifyList() {
@@ -53,16 +55,40 @@ export default function NotifyList() {
   }, [hasMore, isLoading, loadMore]);
 
   return (
-    <ul className="w-full divide-y divide-slate-400/70">
+    <ul className="w-full grid grid-cols-1 gap-1 pt-2">
       {notifications?.map((notification) => (
         <li
           key={notification.id}
-          className="py-2 flex flex-col notification-item"
+          className={clsx(
+            "py-2 px-4 flex flex-col notification-item bg-gray-100 rounded-xl relative"
+          )}
           data-id={notification.id}
           data-read-at={notification.readAt}
         >
+          {!notification.readAt && (
+            <div className="w-3 h-3 rounded-full bg-red-600 absolute -top-[3px] -right-[3px]" />
+          )}
           <div className="self-end text-sm text-slate-500 italic data-[read-at]:bg-slate-600">
-            {formatDate(notification.createdAt, "d MMM yyyy h:mm a")}
+            {moment(notification.createdAt).calendar({
+              sameDay: function () {
+                return (
+                  "[hoy a la" + (this.hours() !== 1 ? "s" : "") + "] hh:mm A"
+                );
+              },
+              lastDay: function () {
+                return (
+                  "[ayer a la" + (this.hours() !== 1 ? "s" : "") + "] hh:mm A"
+                );
+              },
+              lastWeek: function () {
+                return (
+                  "[el] dddd [a la" +
+                  (this.hours() !== 1 ? "s" : "") +
+                  "] hh:mm A"
+                );
+              },
+              sameElse: "d MMM YYYY hh:mm A",
+            })}
           </div>
 
           <p className="flex items-center gap-2">
@@ -75,7 +101,7 @@ export default function NotifyList() {
                 <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
               </svg>
             </span>
-            <span>{parse(notification.content)}</span>
+            <span className="text-sm">{parse(notification.content)}</span>
           </p>
         </li>
       ))}

fa04a29611590bd7bf19889cefff6f77edc3acfd
Yonei Sanchez <yoneisanchez@gmail.com>
2025-01-10 11:41:20 -0400

    notifications

--- src/components/notifier/drawer/NotifyList.jsx
+++ src/components/notifier/drawer/NotifyList.jsx
@@ -15,29 +15,35 @@ export default function NotifyList() {
   const observerRef = useRef(null);
   const loadMoreRef = useRef(null);
 
-  useEffect(() => {
-    if (!observerRef) return;
-    observerRef.current = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          const notificationId = entry.target.getAttribute("data-id");
-          const readAt = entry.target.getAttribute("data-read-at");
+  // useEffect(() => {
+  //   if (!observerRef) return;
+  //   observerRef.current = new IntersectionObserver((entries) => {
+  //     entries.forEach((entry) => {
+  //       if (entry.isIntersecting) {
+  //         const notificationId = entry.target.getAttribute("data-id");
+  //         const readAt = entry.target.getAttribute("data-read-at");
 
-          if (!readAt) {
-            markAsRead(notificationId);
-          }
-        }
-      });
-    });
+  //         if (!readAt) {
+  //           markAsRead(notificationId);
+  //         }
+  //       }
+  //     });
+  //   });
 
-    const elements = document?.querySelectorAll(".notification-item");
-    if (elements)
-      elements?.forEach((element) => observerRef.current.observe(element));
+  //   const elements = document?.querySelectorAll(".notification-item");
+  //   if (elements)
+  //     elements?.forEach((element) => observerRef.current.observe(element));
 
-    return () => {
-      observerRef.current.disconnect();
-    };
-  }, [notifications, markAsRead]);
+  //   return () => {
+  //     observerRef.current.disconnect();
+  //   };
+  // }, [notifications, markAsRead]);
+
+  const handleReadNotification = async (notification, event) => {
+    if (!notification.readAt) {
+      await markAsRead(notification.id);
+    }
+  };
 
   useEffect(() => {
     const loadMoreObserver = new IntersectionObserver((entries) => {
@@ -63,7 +69,7 @@ export default function NotifyList() {
         <li
           key={notification.id}
           className={clsx(
-            "py-2 px-4 flex flex-col notification-item bg-gray-100 rounded-xl relative"
+            "py-2 px-4 flex flex-col group notification-item bg-gray-100 rounded-xl relative"
           )}
           data-id={notification.id}
           data-read-at={notification.readAt}
@@ -71,8 +77,31 @@ export default function NotifyList() {
           {!notification.readAt && (
             <div className="w-3 h-3 rounded-full bg-red-600 absolute -top-[3px] -right-[3px]" />
           )}
-
-          <p className="flex items-center gap-2">
+          <div className="flex justify-end">
+            <div className=" text-sm text-slate-500 italic data-[read-at]:bg-slate-600">
+              {moment(notification.createdAt).calendar({
+                sameDay: function () {
+                  return (
+                    "[hoy a la" + (this.hours() !== 1 ? "s" : "") + "] hh:mm A"
+                  );
+                },
+                lastDay: function () {
+                  return (
+                    "[ayer a la" + (this.hours() !== 1 ? "s" : "") + "] hh:mm A"
+                  );
+                },
+                lastWeek: function () {
+                  return (
+                    "[el] dddd [pasado a la" +
+                    (this.hours() !== 1 ? "s" : "") +
+                    "] hh:mm A"
+                  );
+                },
+                sameElse: "d MMM YYYY hh:mm A",
+              })}
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
             {notification?.metadata?.commentOwnerId ? (
               <Image
                 src={
@@ -100,29 +129,27 @@ export default function NotifyList() {
                 />
               </div>
             )}
-            <div className="text-sm w-10/12">{parse(notification.content)}</div>
-          </p>
-          <div className="self-end text-sm text-slate-500 italic data-[read-at]:bg-slate-600">
-            {moment(notification.createdAt).calendar({
-              sameDay: function () {
-                return (
-                  "[hoy a la" + (this.hours() !== 1 ? "s" : "") + "] hh:mm A"
-                );
-              },
-              lastDay: function () {
-                return (
-                  "[ayer a la" + (this.hours() !== 1 ? "s" : "") + "] hh:mm A"
-                );
-              },
-              lastWeek: function () {
-                return (
-                  "[el] dddd [pasado a la" +
-                  (this.hours() !== 1 ? "s" : "") +
-                  "] hh:mm A"
-                );
-              },
-              sameElse: "d MMM YYYY hh:mm A",
-            })}
+            <div
+              className="text-sm w-10/12"
+              onClickCapture={(event) =>
+                handleReadNotification(notification, event)
+              }
+            >
+              {parse(notification.content)}
+            </div>
+          </div>
+          <div className="flex justify-end">
+            <div
+              className={clsx(
+                "underline text-primary text-xs hidden cursor-pointer",
+                {
+                  "group-hover:block ": !notification?.readAt,
+                }
+              )}
+              onClick={() => markAsRead(notification?.id)}
+            >
+              Marcar como leido
+            </div>
           </div>
         </li>
       ))}

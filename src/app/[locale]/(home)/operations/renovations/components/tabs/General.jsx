168820c2b29fd4825bbbaeb3dd3d00dc41718a1a
yoneisanchez3037 <ysanchez@blindariesgos.com>
2025-02-26 20:28:17 -0400

    Ys/10781 (#382)
    * correccion carga infinita
    
    * Update general.jsx
    
    * mejoras en carga manual
    
    * corregir foto de perfil
    
    * estandarizar lo de beneficiarios en renovaciones
    
    ---------
    
    Co-authored-by: Yonei Sanchez <yoneisanchez@gmail.com>

--- src/app/[locale]/(home)/operations/renovations/components/tabs/General.jsx
+++ src/app/[locale]/(home)/operations/renovations/components/tabs/General.jsx
@@ -136,19 +136,7 @@ export default function PolicyDetails({
         "e1794ba3-892d-4c51-ad62-32dcf836873b", //VIDA
       ].includes(data?.type?.id)
     ) {
-      if (data?.beneficiaries) {
-        setValue("beneficiaries", data?.beneficiaries);
-      } else {
-        setValue("beneficiaries", [
-          {
-            nombre: "",
-            parentesco: "",
-            porcentaje: "",
-            type: "Principal",
-          },
-        ]);
-      }
-      if (data?.insured) {
+      if (data?.insured && data?.insured?.length > 0) {
         setValue("insureds", data?.insured);
       } else {
         setValue("insureds", [
@@ -163,9 +151,23 @@ export default function PolicyDetails({
           },
         ]);
       }
+      if (data?.type?.id == "e1794ba3-892d-4c51-ad62-32dcf836873b") {
+        if (data?.beneficiaries && data?.beneficiaries?.length > 0) {
+          setValue("beneficiaries", data?.beneficiaries);
+        } else {
+          setValue("beneficiaries", [
+            {
+              nombre: "",
+              parentesco: "",
+              porcentaje: "",
+              type: "Principal",
+            },
+          ]);
+        }
+      }
     }
     if (data?.type?.id == "e4e2f26f-8199-4e82-97f0-bdf1a6b6701c") {
-      if (data?.vehicles) {
+      if (data?.vehicles && data?.vehicles?.length > 0) {
         setValue("vehicles", data?.vehicles);
       } else {
         setValue("vehicles", [
